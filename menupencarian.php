c8f48e8cf5eebcc8dfed3fdc4762afd8bf625daf
haykalEka <48209834+haykalEka@users.noreply.github.com>
2019-03-23 09:53:50 +0700

    Add files via upload
    "Pencarian Hasil Penjualan Berdasar Tanggal"

---
+++ menupencarian.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Pegawai</title>
+</head>		
+<body bgcolor="lightblue">
+<h1>Keuntungan Tahun 2017	</h1>
+
+tanggal awal  : <input type="date" name="date1">
+tanggal akhir : <input type="date" name="date2">
+
+
+
+
+<?php 
+
+$date1=$_POST['date1'];
+$date2=$_POST['date2'];
+
+	$QueryString = "SELECT penjualan.tgl_penjualan AS Tanggal_beli, penjualan.no_transaksi AS Invoice, penjualan_detail.kd_barang AS Kode_Barang, nama_obat.nm_barang AS Nama_barang, penjualan_detail.harga AS Harga_satuan, penjualan_detail.jumlah AS Quantity, penjualan_detail.sub_total AS Sub_total FROM penjualan,penjualan_detail,nama_obat WHERE (penjualan.tgl_penjualan BETWEEN $date1 AND $date2) AND penjualan.id=penjualan_detail.id AND nama_obat.kd_barang=penjualan_detail.kd_barang;"
+
+	$SQL = mysqli_query($connect,$QueryString); ?>
+
+	<table border="1">
+		<tr>
+			<th>Harga Barang</th>
+			<th>Tanggal Beli</th>
+			<th>Invoice</th>
+			<th>Kode Barang</th>
+			<th>Nama Barang</th>
+			<th>Harga Barang</th>
+			<th>Quantity</th>
+			<th>Sub Total</th>
+		</tr>
+<?php
+	foreach ($SQL as $data) {
+				echo "
+					<tr>
+						<td>$data[Harga_barang]</td>
+						<td>$data[Tanggal_beli]</td>
+						<td>$data[Invoice]</td>
+						<td>$data[Kode_barang]</td>
+						<td>$data[Nama_barang]</td>
+						<td>$data[Harga_barang]</td>
+						<td>$data[Quantity]</td>
+						<td>$data[Sub_total]</td>
+					</tr>
+				";
+			}
+?>
+</table>
+</body>
+</html>
