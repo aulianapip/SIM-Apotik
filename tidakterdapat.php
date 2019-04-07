afced3ccf105934d3d2628b4117ffb728f10fa26
arindrakurniawan11 <48229794+arindrakurniawan11@users.noreply.github.com>
2019-04-07 20:04:47 +0700

    Add files via upload
    function pencarian data tidak ditemukan

---
+++ tidakterdapat.php
@@ -0,0 +1,97 @@
+<?php
+	include 'db.php';
+	$QuerySql = "SELECT * FROM beli";
+	$SQL = mysqli_query($connect, $QuerySql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tampil Data Pembelian</title>
+	<link rel="stylesheet" href="bulma.min.css">
+</head>
+<body>
+<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+   
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="login.html">
+    <img src="logut.png"></img>
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Daftar Tabel
+        </a>
+
+        <div class="navbar-dropdown">
+          
+          <a class="navbar-item" href="dataobat.php">
+            Obat
+          </a>
+          <a class="navbar-item" href="datasupiler.php">
+            Supiler
+          </a>
+    
+        </div>
+      </div>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Sort By
+        </a>
+
+        <div class="navbar-dropdown">
+          
+          <a class="navbar-item" href="dataobattanggal.php">
+            Tanggal
+          </a>
+          <a class="navbar-item" href="dataobatnama.php">
+            Nama
+          </a>
+          <a class="navbar-item" href="dataobatmurah.php">
+            Harga Termurah
+          </a>
+          <a class="navbar-item" href="dataobatmahal.php">
+            Harga Termahal
+          </a>
+    
+        </div>
+      </div>
+
+
+  <a class="navbar-item" href="inputobat.php">
+       Tambah Obat
+      </a>
+       <a class="navbar-item" href="inputsupiler.php">
+       Tambah Suppiler
+      </a>
+      <a class="navbar-item">
+        <form action="cariobat.php" method="post">
+        <div class="control">
+          <input class="input" type="text" placeholder="CARI OBAT" name="cari"> 
+        </div>
+</div>
+        </form>
+  </a>
+  </div>
+</div>
+    </div>
+
+    
+  </div>
+
+</nav>
+<table class="table is-fullwidth" >
+<h3 align="center">TIDAK TERDAPAT DATA</h3>
+</body>
+</html>
