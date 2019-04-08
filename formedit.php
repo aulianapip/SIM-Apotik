014b6f2b7ef6a864de8f8877e1fa161e99507b0e
adeliazakiyyah29 <adeliafitriawati29@gmail.com>
2019-04-08 14:04:01 +0700

    modifikasi source code punya rizka yang formedit.php

--- formedit.php
+++ formedit.php
@@ -3,30 +3,26 @@
 		include 'conect.php';
 		$ID = $_POST['ID'];
 		$Nama = $_POST['Nama'];
-		$JK = $_POST['JK'];
+		$JK = $_POST['Jk'];
 		$NoHP = $_POST['NoHP'];
 		$Alamat = $_POST['Alamat'];
 		$Email = $_POST['Email'];
-		$Member = $_POST['Member'];
 
-		$QuerySql = "UPDATE pelanggan SET ID='$ID', Nama='$Nama', JK='$JK', NoHP='$NoHP', Email='$Email', Alamat='$Alamat', Member='$Member' WHERE ID='$ID'";
+		$QuerySql = "UPDATE pelanggan SET ID='$ID', Nama='$Nama', JK='$JK', NoHP='$NoHP', Email='$Email', Alamat='$Alamat' WHERE ID='$ID' ";
 		$SQL = mysqli_query($connect,$QuerySql);
 
-		echo " Input Berhasil <br>";
+		echo " Edit Berhasil <br>";
 		echo "<a href='pelanggan.php'>Lihat Hasil</a> ";
 	}
-
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>	
-	<title>Edit User Data</title>
+	<title>EDIT DATA PELANGGAN</title>
 </head>
 <body>
 	<form method="post" action="formedit.php"><center>
-		<b><h1><center><font face="Bebas" size="">EDIT DATA</font></h1></b>
+		<b><h1><center><font face="Bebas" size="">EDIT DATA PELANGGAN</font></h1></b>
 		<br>
 		<table>
 		<tr><td>ID</td>
@@ -37,7 +33,9 @@
 		<td><input class="form-control" type="text" name="Nama" value=""></td></tr>
 		<tr><td>JK</td>
 		<td> : </td>
-		<td><input class="form-control" type="text" name="JK" value=""></td></tr>
+		<td><select name="Jk" >
+			<option>Laki-Laki</option>
+			<option>Perempuan</option></tr>
 		<tr><td>NoHP</td>
 		<td> : </td>
 		<td><input class="form-control" type="text" name="NoHP" value=""></td></tr>
@@ -47,18 +45,13 @@
 		<tr><td>Alamat</td>
 		<td> : </td>
 		<td><input class="form-control" type="text" name="Alamat" value=""></td></tr>
-		<tr><td>Member</td>
-		<td> : </td>
-		<td><select name="Member" value="">
-			<option>Member</option>
-			<option>Non Member</option>
-		</select></td></tr>
-
+	
 	</table>
 	<br>
-	<button type="submit" class="btn btn-dark" >SIMPAN</button>
+	<button type="submit"  class="btn btn-dark" >SEND</button>
 	</center>
 </form>
 </body></html>
 </body>
 </html>
+
