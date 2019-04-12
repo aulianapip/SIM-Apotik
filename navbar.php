15ea930c1ed8eaaae7914257d69da6a82007de94
wais46 <w.alqorni240@gmail.com>
2019-04-13 00:01:59 +0700

    Home (Navbar)

---
+++ navbar.php
@@ -0,0 +1,57 @@
+<!--WAIS ALQORNI-->
+<head>
+  <title>Tampil Data Obat</title>
+  <link rel="stylesheet" href="bulma.min.css">
+</head>
+<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+   
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          <img src="admin.png">
+        </a>
+        <div class="navbar-dropdown">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/logout.php">
+            Logout
+          </a>
+        </div>
+      </div>
+        <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Keuangan
+        </a>
+        <div class="navbar-dropdown">
+          <a class="navbar-item" href="data_pembelian.php">
+            Data Pembelian
+          </a>
+          <a class="navbar-item" href="data_penjualan.php">
+            Data Penjualan
+          </a>
+          <a class="navbar-item" href="total_keuntungan.php">
+            Total Keuntungan
+          </a>
+                    </a>
+          <a class="navbar-item" href="index.php">
+            Cash Flow
+          </a>    
+        </div>
+      </div>
+     
+      </div>
+      
+  </div>
+</div>
+    </div>
+
+    
+  </div>
+
+</nav>
