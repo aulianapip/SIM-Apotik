447f701393a87ebae396fdb06c3efe16b31da5f9
fesus19 <febri1600018078@webmail.uad.ac.id>
2019-04-13 00:20:23 +0700

    sorting penjualan berdasarkan minggu & tahun

---
+++ penjualan_minggu.php
@@ -0,0 +1,48 @@
+<!--FEBRI-->
+<?php
+	session_start();
+
+if (!isset($_SESSION["login1"])) {
+    	  header("location: http://localhost/apotik-keuangan/login.php");
+      exit;
+    }
+      
+  
+	include "connection/db.php";
+	$QuerySql = "SELECT *,sum(harga_obat) as harga_obat,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  week(tanggal_terjual)";
+
+	$SQL = mysqli_query($connect, $QuerySql); 
+?> 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tampil Data Obat</title>
+	<link rel="stylesheet" href="bulma.min.css">
+</head>
+<body>
+<?php 
+  include "navbar/navbar_penjualan.php";
+ ?>
+<table class="table is-fullwidth" >
+  <thead>
+    <tr>
+      <th scope="col">TANGGAL</th>
+      <th scope="col">HARGA OBAT</th>
+      <th scope="col">JUMLAH TERJUAL</th>
+      <th scope="col">TOTAL PENJUALAN</th>
+    </tr>
+  </thead>
+		<?php
+			foreach ($SQL as $key) {
+				echo "<tr>
+						<td>$key[tanggal_terjual]</td>
+						<td>$key[harga_obat]</td>
+						<td>$key[jumlah_terjual]</td>
+						<td>$key[total]</td>
+				</tr>";
+                	
+				}
+		?>
+</table>
+</body>
+</html>
