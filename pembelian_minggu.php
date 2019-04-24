65f71c9d7fc250a0c313a7c4abe58afb0cdb3fcf
rekarachmadi21 <rekarachmadi23@gmail.com>
2019-04-25 01:25:30 +0700

    UTS_REKARACHMADI_1700018237

--- pembelian_minggu.php
+++ pembelian_minggu.php
@@ -1,16 +1,24 @@
 <!--REKA RACHMADI APRIANSYAH-->
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
 
 <?php
-  session_start();
-
-if (!isset($_SESSION["login1"])) {
-        header("location: http://localhost/apotik-keuangan/login.php");
-      exit;
+  session_start();// untuk memulai eksekusi session pada server dan kemudian menyimpannya pada browser dan posisinya
+                  // harus paling depan.
+if (!isset($_SESSION["login1"])) {//Jika belum melakukan login makan akan dilempar ke header
+      header("location: http://localhost/apotik-keuangan/home.php");//di header ini adalah link untuk kembali ke home
+      exit; //keluar
     }
-  include "connection/db.php";
-  $QuerySql = "SELECT *,sum(jumlah_obat) as jumlah_obat,sum(harga_beli) as harga_beli,sum(harga_beli*jumlah_obat) AS total FROM `supplier`, `obat` WHERE supplier.kode_obat=obat.kode_obat GROUP BY week(tanggal_beli) ASC";
+    include 'connection/db.php'; //sebagai database yang akan dipake
+  $QuerySql = "SELECT *,sum(jumlah_obat) as jumlah_obat,sum(harga_beli) as harga_beli,sum(harga_beli*jumlah_obat) AS total FROM `supplier`, `obat` WHERE supplier.kode_obat=obat.kode_obat GROUP BY week(tanggal_beli) ASC";//Variabel QuerySql menampung query untuk menampilkan jumlah_obat yang telah di jumlahkan yang diberi nama inisial jumlah_obat,harga_beli yang telah di jumlahkan yang diberi nama inisial harga_beli, mengalikan harga_beli dengan jumlah_beli kemudiandi jumlahkan dan diberi nama inisial total , kemudian di shorting berdasakan tanggal_beli mingguan
 
-  $SQL = mysqli_query($connect, $QuerySql); 
+  $SQL = mysqli_query($connect, $QuerySql); //variabel cari2 ini menampung berupa perintah query pada variabel cari ke server yang berada di variabel connect
 ?> 
 <!DOCTYPE html>
 <html>
@@ -32,7 +40,7 @@ if (!isset($_SESSION["login1"])) {
     </tr>
   </thead>
     <?php
-      foreach ($SQL as $key) {
+      foreach ($SQL as $key) {//melakukan fungsi looping untuk menampilkan tanggal_beli,harga beli,jumlah_obat dan total
         echo "<tr>
             <td>$key[tanggal_beli]</td>
             <td>$key[harga_beli]</td>
