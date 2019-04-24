350baf3dcf7463c109979ba43f83d8ece30c1929
Mohamadrifkyfajri <48209649+Mohamadrifkyfajri@users.noreply.github.com>
2019-04-24 16:46:15 +0700

    Mohamad rifky fajri 1700018239 UTS PRPL
    function mencari data supplier

--- carisuppiler.php
+++ carisuppiler.php
@@ -1,7 +1,26 @@
+<!--1.  Pada kelompok gudang, kami telah membuat beberapa kelas yang mempunyai fungsi sebagai berikut:
+• Fungsi Sorting Nama Obat A-Z : fitur ini berfungsi mengurutkan nama obat sesuai alpabet dari awalan huruf A sampai awalan huruf Z.
+• Fungsi Sorting Jenis Obat Kapsul : fitur ini berfungsi mensorting obat yang berjenis kapsul untuk di tampilkan.
+• Fungsi Menampilkan seluruh data obat
+• Fungsi Tanggal pasok obat
+• Function Update Data Obat : Fitur ini berfungsi mengupdate perubahan yang telah kita tambah, Kurang, dan mengedit sesuai database
+• Function Stok Obat Menipis : Fitur ini berfungsi menandai tabel obat yang stoknya telah menipis
+• Function Pencarian  Data Tidak Ditemukan : fitur ini berfungsi  jika kita mencari obat atau supplier yang tidak ada didatabase
+• Function Sorting Tanggal Supplier : fitur ini berfungsi mensorting obat dengan tanggal pemasok supplier yang telah memasok obat dari tanggal terdahulu
+• Function Tambah Obat : fitur ini berfungsi untuk menambah data obat baru ke dalam tabel Obat
+• Function Tambah Supllier : fitur ini berfungsi untuk menambah data supllier baru ke tabel Supllier
+• Function Cari Obat : fitur ini berfungsi buat mencari data obat yang ada di tabel obat
+• Fuction Cari Supplier : fitur ini berfungsi mncari data supplier yang ada di tabel supplier
+• Function Sorting Nama Supplier A-Z : fitur ini berfungsi untuk mengurutkan nama supplier dari A-Z
+• Function data suplier : fitur ini berfungsi untuk menampilkan data suplier sesuai database
+-->
 <?php
   include 'db.php';
   $carisupp = $_POST['carisupp'];
   $QuerySql = "SELECT `nama_pemasok`,`kode_obat`,`jumlah_pasok`,`nomer_telepon_supp`,`kode_pasok`,`harga_beli`,`tanggal_pasok` FROM`supplier` WHERE nama_pemasok LIKE '%$carisupp%'";
+  //query untuk menampilkan data supplier yang dicari ditabel supplier, dimana jika kita ketik A di kolom pencarian maka akan muncul data supplier yang ada huruf A nya, tentu data supplier yang ada di database yang muncul
+  //Mohamad rifky fajri 1700018239 
+
   $SQL = mysqli_query($connect, $QuerySql);
       if(mysqli_num_rows($SQL)>0){
 ?>
