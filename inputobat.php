7489560f306d92e3a199a74b0e8bc6d0ec5784d6
Sultan Agam <48223826+sultanagam@users.noreply.github.com>
2019-04-24 16:49:06 +0700

    M. Rafie Sultan Agam - 1700018282 - UTS PRPL
    Membuat Function Menambah Data Obat

--- inputobat.php
+++ inputobat.php
@@ -14,11 +14,31 @@
 
     $QuerySql = " INSERT INTO obat VALUES ('$nama_obat', '$harga_obat', '$kode_obat', '$dosis_obat', '$kode_jenis', '$tanggal_kadaluarsa', '$bulan_kadaluarsa','$tahun_kadaluarsa', '$Stok_Obat','$tanggal_input')";
       $SQL = mysqli_query($connect, $QuerySql);
+	//Query ini untuk menambah data obat baru ke dalam tabel Obat
+      	//M. Rafie Sultan Agam 1700018282 
 
   }
 
 ?>
 <!--M . Rafie Sultan Agam-->
+<!--M . Rafie Sultan Agam-->
+<!--1.  Pada kelompok gudang, kami telah membuat beberapa kelas yang mempunyai Fungsi sebagai berikut:
+• Fungsi Sorting Nama Obat A-Z : fitur ini berfungsi mengurutkan nama obat sesuai alpabet dari awalan huruf A sampai awalan huruf Z.
+• Fungsi Sorting Jenis Obat Kapsul : fitur ini berfungsi mensorting obat yang berjenis kapsul untuk di tampilkan.
+• Fungsi Menampilkan seluruh data obat
+• Fungsi Tanggal pasok obat
+• Function Update Data Obat : Fitur ini berfungsi mengupdate perubahan yang telah kita tambah, Kurang, dan mengedit sesuai database
+• Function Stok Obat Menipis : Fitur ini berfungsi menandai tabel obat yang stoknya telah menipis
+• Function Pencarian  Data Tidak Ditemukan : fitur ini berfungsi  jika kita mencari obat atau supplier yang tidak ada didatabase
+• Function Sorting Tanggal Supplier : fitur ini berfungsi mensorting obat dengan tanggal pemasok supplier yang telah memasok obat dari tanggal terdahulu
+• Function Tambah Obat : fitur ini berfungsi untuk menambah data obat baru ke dalam tabel Obat
+• Function Tambah Supllier : fitur ini berfungsi untuk menambah data supllier baru ke tabel Supllier
+• Function Cari Obat : fitur ini berfungsi buat mencari data obat yang ada di tabel obat
+• Fuction Cari Supplier : fitur ini berfungsi mncari data supplier yang ada di tabel supplier
+• Function Sorting Nama Supplier A-Z : fitur ini berfungsi untuk mengurutkan nama supplier dari A-Z
+• Function data suplier : fitur ini berfungsi untuk menampilkan data suplier sesuai database
+• Function sorting obat mahal : fitur ini berfungsi untuk mengurutkan harga obat dari yang termahal
+-->
 <!DOCTYPE html>
 <html>
 <head>
