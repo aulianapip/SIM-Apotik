11fbf0642752fcd1202c77b91b743617b32ff92a
harunsetiaji23 <harunsetiaji8@gmail.com>
2019-04-25 01:48:14 +0700

    UTS Harun Setiaji 1700018271

--- kasir/faktur_penjualan.php
+++ kasir/faktur_penjualan.php
@@ -1,29 +1,43 @@
 <?php
+
+//Fitur POS, fitur untuk menjalankan fungsi kasir dan operasional penjualan apotik 
+
+// Nama  : Harun Setiaji
+// NIM   : 1700018271
+// Kelas : E
+
+//fungis untuk pengeluaran nota 
+
 if(session_status()!==2)session_start();//>=php 5.4
+
 if(!isset($_SESSION['SES_LOGIN'])){
   header('location:../home');
  }
+
+ //library untuk koneksi ke database
 require_once "../library/inc.connection.php";
 require_once "../library/inc.library.php";
+
 $user_id=$_SESSION['SES_LOGIN'];
 opendb();
 
 if(isset($_GET['no_faktur'])){
   
   
+  // untuk mengambil data dari hasil inputan
   $noFaktur=trim($_GET['no_faktur']); 
   $cash = trim($_GET['cash']);
   
-  //cari data Toko 
+  //cari data di Toko  
   $qri = "SELECT * FROM nama_toko";
-  $hsl = querydb($qri);
+  $hsl = querydb($qri); 
   $rek = arraydb($hsl);
     $namaToko = $rek['nm_toko'];
     $almtToko = $rek['almt_toko'];
     $tlpToko  = $rek['tlp_toko'];
     $faxToko  = $rek['fax_toko'];
 
-  //cari data penjualan dari tabel penjualan_tp
+  //mencari data penjualan dari tabel penjualan_tp
   $qri2 = "SELECT a.*,b.nm_barang FROM penjualan_tmp a LEFT JOIN nama_obat b ON b.kd_barang=a.kd_barang WHERE a.no_faktur='$noFaktur'";
   $hsl2 = querydb($qri2); 
     
@@ -34,12 +48,13 @@ if(isset($_GET['no_faktur'])){
     
 }
 
-?>
+// Berikut ini adalah tampilan dari nota kasir apotik
+?> 
 <!DOCTYPE html>
 <html lang="id">
 <head>
 <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Nota</title>
   <!-- Bootstrap -->
@@ -64,7 +79,7 @@ if(isset($_GET['no_faktur'])){
 
 <table border="0" align="center">
   <tr>
-    <td colspan="4" align="center"><?php echo $namaToko."<br>".$almtToko."<br>Telp.: ".$tlpToko."&nbsp;Fax :&nbsp;".$faxToko; ?>&nbsp;</td>
+    <td colspan="4" align="center"><?php echo $namaToko."<br>".$almtToko."<br>Telp.: ".$tlpToko."&nbsp;Fax :&nbsp;".$faxToko; ?>&nbsp;</td>           //output data
   </tr>
 </table>
 <table border="0" align="center">
@@ -103,8 +118,9 @@ if(isset($_GET['no_faktur'])){
     <td><?php echo $jml; ?>&nbsp;</td>
     <td><?php echo number_format($hrg,0,",","."); ?>&nbsp;</td>
     <td><?php echo number_format($sub_total,0,",","."); ?></td>
-  </tr>
- <?php } $kembali=$cash-$total_penjualan; if($kembali<=0){$kembali=0;} ?>
+  </tr>                                                                 
+                                                                                                                       // Rumus dari hasil kembali                                                                                        
+ <?php } $kembali=$cash-$total_penjualan; if($kembali<=0){$kembali=0;} ?>                                            
  <tr>
     <td colspan="4">----------------------------------------------------------------------------------------------------</td>
   </tr>
@@ -112,7 +128,7 @@ if(isset($_GET['no_faktur'])){
     <td>TOTAL</td>
     <td><?php echo $total_item; ?>&nbsp;ITEM</td>
     <td>&nbsp;</td>
-    <td><?php echo number_format($total_penjualan,0,",","."); ?></td>
+    <td><?php echo number_format($total_penjualan,0,",","."); ?></td>                                                //menampilkan total biaya yang
   </tr>
   <tr>
     <td colspan="4">----------------------------------------------------------------------------------------------------</td>
@@ -121,13 +137,13 @@ if(isset($_GET['no_faktur'])){
     <td>&nbsp;</td>
     <td>&nbsp;</td>
     <td>TUNAI</td>
-    <td><?php echo number_format($cash,0,",","."); ?></td>
+    <td><?php echo number_format($cash,0,",","."); ?></td>                                                          //menampilkan uang yang di bayarkan
   </tr>
   <tr>
     <td>&nbsp;</td>
     <td>&nbsp;</td>
     <td>KEMBALI</td>
-    <td><?php echo number_format($kembali,0,",","."); ?></td>
+    <td><?php echo number_format($kembali,0,",","."); ?></td>                                                       //kembali ke menu
   </tr>
 
   <tr>
