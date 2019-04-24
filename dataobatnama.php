856e0c71dace547d814d5d0319d17a38e8ebfe50
putraaditya1877 <48209579+putraaditya1877@users.noreply.github.com>
2019-04-24 19:31:50 +0700

    Aditya Gusti Mandala Putra 1700018256 UTS PRPL

--- dataobatnama.php
+++ dataobatnama.php
@@ -1,7 +1,27 @@
+<!--1.  Pada kelompok gudang, kami telah membuat beberapa kelas yang mempunyai fungsi sebagai berikut:
+• Fungsi Sorting Nama Obat A-Z : fitur ini berfungsi mengurutkan nama obat sesuai alpabet dari awalan huruf A sampai awalan huruf Z.
+• Fungsi Sorting Jenis Obat Kapsul : fitur ini berfungsi mensorting obat yang berjenis kapsul untuk di tampilkan.
+• Fungsi Menampilkan seluruh data obat
+• Fungsi Tanggal pasok obat
+• Function Update Data Obat : Fitur ini berfungsi mengupdate perubahan yang telah kita tambah, Kurang, dan mengedit sesuai database
+• Function Stok Obat Menipis : Fitur ini berfungsi menandai tabel obat yang stoknya telah menipis
+• Function Pencarian  Data Tidak Ditemukan : fitur ini berfungsi  jika kita mencari obat atau supplier yang tidak ada didatabase
+• Function Sorting Tanggal Supplier : fitur ini berfungsi mensorting obat dengan tanggal pemasok supplier yang telah memasok obat dari tanggal terdahulu
+• Function Tambah Obat : fitur ini berfungsi untuk menambah data obat baru ke dalam tabel Obat
+• Function Tambah Supllier : fitur ini berfungsi untuk menambah data supllier baru ke tabel Supllier
+• Function Cari Obat : fitur ini berfungsi buat mencari data obat yang ada di tabel obat
+• Fuction Cari Supplier : fitur ini berfungsi mncari data supplier yang ada di tabel supplier
+• Function Sorting Nama Supplier A-Z : fitur ini berfungsi untuk mengurutkan nama supplier dari A-Z
+• Function data suplier : fitur ini berfungsi untuk menampilkan data suplier sesuai database
+• Function sorting obat mahal : fitur ini berfungsi untuk mengurutkan harga obat dari yang termahal
+-->
+
 <?php
 	include 'db.php';
 	$QuerySql = "SELECT * FROM `obat`, `jenis_obat` WHERE jenis_obat.kode_jenis=obat.kode_jenis ORDER BY `nama_obat` ASC";
 	$SQL = mysqli_query($connect, $QuerySql);
+	//query untuk menampilkan data obat berdasarkan urutan huruf alpabet dari awalan huruf A sampai awalan huruf Z. kita "Select" tabel obat dan jenis obat, selanjutnya kita masukan kondisi jenis obat beserta kodenya lalu kita "Order by" nama obat secara Ascending
+  	//Aditya Gusti Mandala Putra 1700018256  
 ?>
 <!--Aditya Gusti Mandala Putra-->
 
