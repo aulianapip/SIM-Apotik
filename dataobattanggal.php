4d50e9aaf84b271257d28e70d06be1c507903fe3
afrizalagam <48230205+afrizalagam@users.noreply.github.com>
2019-04-24 20:00:15 +0700

    MUHAMMAD AFRIZAL 1700018231 UTS PRPL
    function untuk menampilkan data obat berdasarkan tanggal inputnya

--- dataobattanggal.php
+++ dataobattanggal.php
@@ -1,8 +1,27 @@
+<!--1.  Pada kelompok gudang, kami telah membuat beberapa kelas yang mempunyai fungsi sebagai berikut:
+• Fungsi Sorting Nama Obat A-Z : fitur ini berfungsi mengurutkan nama obat sesuai alpabet dari awalan huruf A sampai awalan huruf Z.
+• Fungsi Sorting Jenis Obat Kapsul : fitur ini berfungsi mensorting obat yang berjenis kapsul untuk di tampilkan.
+• Fungsi Menampilkan seluruh data obat
+• Fungsi Tanggal pasok obat
+• Function Update Data Obat : Fitur ini berfungsi mengupdate perubahan yang telah kita tambah, Kurang, dan mengedit sesuai database
+• Function Stok Obat Menipis : Fitur ini berfungsi menandai tabel obat yang stoknya telah menipis
+• Function Pencarian  Data Tidak Ditemukan : fitur ini berfungsi  jika kita mencari obat atau supplier yang tidak ada didatabase
+• Function Sorting Tanggal Supplier : fitur ini berfungsi mensorting obat dengan tanggal pemasok supplier yang telah memasok obat dari tanggal terdahulu
+• Function Tambah Obat : fitur ini berfungsi untuk menambah data obat baru ke dalam tabel Obat
+• Function Tambah Supllier : fitur ini berfungsi untuk menambah data supllier baru ke tabel Supllier
+• Function Cari Obat : fitur ini berfungsi buat mencari data obat yang ada di tabel obat
+• Fuction Cari Supplier : fitur ini berfungsi mncari data supplier yang ada di tabel supplier
+• Function Sorting Nama Supplier A-Z : fitur ini berfungsi untuk mengurutkan nama supplier dari A-Z
+• Function data suplier : fitur ini berfungsi untuk menampilkan data suplier sesuai database
+-->
 <?php
 	include 'db.php';
 	$QuerySql = "SELECT * FROM `obat`, `jenis_obat` WHERE jenis_obat.kode_jenis=obat.kode_jenis ORDER BY `tanggal_input` ASC";
 	$SQL = mysqli_query($connect, $QuerySql);
+//query untuk menampilkan data obat berdasarkan tanggal inputnya, mengurutkan berdasarkan tanggal input dari awal obat yg masuk sampai obat yg terakhir di input
+//Muhammad Afrizal / 1700018231
 ?>
+
 
 <!--Muhammad Afrizal-->
 
