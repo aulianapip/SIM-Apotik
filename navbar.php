49fe4bff2de774d53211b4cd356ac20c3b8062fc
wais46 <w.alqorni240@gmail.com>
2019-04-25 07:20:54 +0700

    Home (Navbar & Navbar_cashflow)

--- navbar.php
+++ navbar.php
@@ -1,9 +1,21 @@
 <!--WAIS ALQORNI-->
+<!--1700018240-->
+
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
+
+
 <head>
   <title>Tampil Data Obat</title>
-  <link rel="stylesheet" href="bulma.min.css">
+  <link rel="stylesheet" href="bulma.min.css"> <!--pemanggilan library css untuk mempercantik tampilan-->
 </head>
-<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+<nav class="navbar is-success" role="navigation" aria-label="main navigation"> <!--fungsi untuk mempercantik tampilan navigasi bar di atas-->
   <div class="navbar-brand">
    
 
@@ -14,33 +26,33 @@
     </a>
   </div>
 
-  <div class="navbar-item has-dropdown is-hoverable">
+  <div class="navbar-item has-dropdown is-hoverable"> <!--Untuk membuat daftar dari menu-->
         <a class="navbar-link">
-          <img src="admin.png">
+          <img src="admin.png">  <!--mengubah latar pada navbar-->
         </a>
         <div class="navbar-dropdown">
           <a class="navbar-item" href="http://localhost/apotik-keuangan/logout.php">
-            Logout
+            Logout <!--Mempercantik tampilan pada logout-->
           </a>
         </div>
       </div>
-        <div class="navbar-item has-dropdown is-hoverable">
+        <div class="navbar-item has-dropdown is-hoverable"> 
         <a class="navbar-link">
-          Keuangan
+          Keuangan <!--isi dari daftar-->
         </a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="data_pembelian.php">
-            Data Pembelian
+          <a class="navbar-item" href="data_pembelian.php"> <!--Hyperlink ke bagian data pembelian-->
+            Data Pembelian <!--isi dari daftar-->
           </a>
-          <a class="navbar-item" href="data_penjualan.php">
-            Data Penjualan
+          <a class="navbar-item" href="data_penjualan.php"> <!--Hyperlink ke bagian data penjualan-->
+            Data Penjualan <!--isi dari daftar-->
           </a>
-          <a class="navbar-item" href="total_keuntungan.php">
-            Total Keuntungan
+          <a class="navbar-item" href="total_keuntungan.php"> <!--Hyperlink ke bagian total keuntungan-->
+            Total Keuntungan <!--isi dari daftar-->
           </a>
                     </a>
-          <a class="navbar-item" href="index.php">
-            Cash Flow
+          <a class="navbar-item" href="index.php"> <!--Hyperlink ke bagian cashflow-->
+            Cash Flow <!--isi dari daftar-->
           </a>    
         </div>
       </div>
--- navbar_cashflow.php
+++ navbar_cashflow.php
@@ -1,9 +1,24 @@
 <!--WAIS ALQORNI-->
+
+
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
+
+
+
+
+<!--Ini codingan untuk memunculkan kembali navbar pada page cashflow-->
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="edit.css">
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="edit.css"> <!--memanggil css file-->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> <!--memanggil css file-->
   <script type="text/javascript" src="js/jquery.js"></script>
   <script type="text/javascript" src="js/bootstrap.js"></script>
 </head>
@@ -21,7 +36,7 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item" href="home.php">
+      <a class="navbar-item" href="home.php"> <!--hyperlink untuk kembali ke home-->
        Kembali
       </a>
   </div>
