7bd1e718a5f1250c7c3b23dee6edff592f2a0c5a
thobiedjailani@gmail.com <alwanzakii@gmail.com>
2019-04-25 10:07:01 +0700

    penjualan periode bulan

--- penjualan_bulan.php
+++ penjualan_bulan.php
@@ -1,17 +1,26 @@
 <!-- THOBIE ZATONI -->
+<!-- 1700018241-->
+
+<!-- Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+   -->
 <?php
 	session_start();
 
-if (!isset($_SESSION["login1"])) {
-    	  header("location: http://localhost/apotik-keuangan/login.php");
+if (!isset($_SESSION["login1"])) {//jika login gagal maka kembali login.php
+    	  header("location: http://localhost/apotik-keuangan/login.php");//link untuk login.php
       exit;
     }
       
   
 	include "connection/db.php";
-	$QuerySql = "SELECT *,sum(jumlah_terjual) as jumlah_terjual,sum(harga_obat) as harga_obat,sum(harga_obat)*sum(jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  month(tanggal_terjual)";
+	$QuerySql = "SELECT *,sum(jumlah_terjual) as jumlah_terjual,sum(harga_obat) as harga_obat,sum(harga_obat)*sum(jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  month(tanggal_terjual)";//fungsi untuk memanggil [total jumlah terjual , harga jual , harga obat berdasarkan bulan terjual
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql); //inisialisasi SQL
 ?> 
 <!DOCTYPE html>
 <html>
@@ -39,7 +48,7 @@ if (!isset($_SESSION["login1"])) {
 						<td>$key[harga_obat]</td>
 						<td>$key[jumlah_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";
+				</tr>";//menampilkan isi dari atribut - atribut di dalam tabel
                 	
 				}
 		?>
