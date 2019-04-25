2186949afac979fcabbf162150efd675ca59c267
vikriammar <vikri2607@gmail.com>
2019-04-25 09:26:17 +0700

    Design navbar pembelian & penjualan

--- navbar/navbar_pembelian.php
+++ navbar/navbar_pembelian.php
@@ -1,13 +1,21 @@
 <!-- VIKRI AMMAR KHOLIS-->
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
 <head>
   <title>Tampil Data Obat</title>
-  <link rel="stylesheet" href="bulma.min.css">
+  <link rel="stylesheet" href="bulma.min.css"> <!--Untuk Connect ke File CSS-->
 </head>
-<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+<nav class="navbar is-success" role="navigation" aria-label="main navigation"> <!-- untuk membentuk wadah navbar-->
   <div class="navbar-brand">
    
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"><!-- Menamplkan menu navbar-->
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -16,33 +24,33 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item" href="home.php">Kembali</a>
-      <a class="navbar-item" href="data_pembelian.php">Data Pembelian</a>
-      <a class="navbar-item" href="pembelian_obat.php">Beli Obat</a>
+      <a class="navbar-item" href="home.php">Kembali</a> <!-- untuk connect ke file HOME.php-->
+      <a class="navbar-item" href="data_pembelian.php">Data Pembelian</a><!-- untuk connect ke file data_pembelian.php-->
+      <a class="navbar-item" href="pembelian_obat.php">Beli Obat</a><!-- untuk connect ke file pembelian_obat.php-->
        <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">
           Total Pembelian Berdasarkan Periode
         </a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_hari.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_hari.php"> <!-- untuk connect ke pembelian berdasarkan hari-->
             Hari
           </a>
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_minggu.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_minggu.php"><!-- untuk connect ke pembelian berdasarkan Minggu-->
             Minggu
           </a>
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_bulan.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_bulan.php"><!-- untuk connect ke pembelian berdasarkan Bulan-->
             Bulan
           </a>
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_tahun.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/pembelian_tahun.php"><!-- untuk connect ke pembelian berdasarkan Tahun-->
             Tahun
           </a>
         </div>
       </div>
 
     <a class="navbar-item">
-        <form action="http://localhost/apotik-keuangan/Administration/pembelian_cari.php" method="post">
+        <form action="http://localhost/apotik-keuangan/Administration/pembelian_cari.php" method="post"> <!-- untuk Kolom Pencarian-->
         <div class="control">
-          <input class="input" type="text" placeholder="Cari Obat" name="cari"> 
+          <input class="input" type="text" placeholder="Cari Obat" name="cari"> <!-- untuk form inputan-->
         </div>
         </form>
   </a>
--- navbar/navbar_penjualan.php
+++ navbar/navbar_penjualan.php
@@ -1,13 +1,21 @@
 <!-- VIKRI AMMAR KHOLIS-->
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
 <head>
   <title>Tampil Data Obat</title>
-  <link rel="stylesheet" href="bulma.min.css">
+  <link rel="stylesheet" href="bulma.min.css"><!--Untuk Connect ke File CSS-->
 </head>
-<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+<nav class="navbar is-success" role="navigation" aria-label="main navigation"><!-- untuk membentuk wadah navbar-->
   <div class="navbar-brand">
    
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample"><!-- Menamplkan menu navbar-->
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -16,32 +24,32 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item" href="home.php">Kembali</a>
-      <a class="navbar-item" href="data_penjualan.php">Data Penjualan</a>
+      <a class="navbar-item" href="home.php">Kembali</a> <!-- untuk connect ke file HOME.php-->
+      <a class="navbar-item" href="data_penjualan.php">Data Penjualan</a><!-- untuk connect ke file data_penjualan.php-->
        <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">
           Total Penjualan Berdasarkan Periode
         </a>
         <div class="navbar-dropdown">
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_hari.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_hari.php"><!-- untuk connect ke penjualan berdasarkan hari-->
             Hari
           </a>
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_minggu.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_minggu.php"><!-- untuk connect ke penjualan berdasarkan Minggu-->
             Minggu
           </a>
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_bulan.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_bulan.php"><!-- untuk connect ke penjualaan berdasarkan Bulan-->
             Bulan
           </a>
-          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_tahun.php">
+          <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/penjualan_tahun.php"><!-- untuk connect ke penjualan berdasarkan Tahun-->
             Tahun
           </a>
         </div>
       </div>
 
     <a class="navbar-item">
-        <form action="http://localhost/apotik-keuangan/Administration/penjualan_cari.php" method="post">
+        <form action="http://localhost/apotik-keuangan/Administration/penjualan_cari.php" method="post"><!-- untuk Kolom Pencarian-->
         <div class="control">
-          <input class="input" type="text" placeholder="Cari Obat" name="cari"> 
+          <input class="input" type="text" placeholder="Cari Obat" name="cari"> <!-- untuk form inputan-->
         </div>
         </form>
   </a>
