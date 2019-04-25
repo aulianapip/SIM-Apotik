d24b1314c69bfce91d84c2ef43b2c4e49852488d
fesus19 <febri1600018078@webmail.uad.ac.id>
2019-04-25 09:18:13 +0700

    UTS 1600018078 FEBRI

--- penjualan_tahun.php
+++ penjualan_tahun.php
@@ -1,22 +1,30 @@
-<!--FEBRI-->
+<!--FEBRI SUSENO-->
+<!--1600018078-->
+<!-- Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+   -->
 <?php
-	session_start();
+	session_start();// untuk melakukan aktifitas yang berhubungan dengan interaksi user pada sebuah web server php.
 
-if (!isset($_SESSION["login1"])) {
-    	  header("location: http://localhost/apotik-keuangan/login.php");
+if (!isset($_SESSION["login1"])) { //jika login gagal maka kembali ke login.php
+    	  header("location: http://localhost/apotik-keuangan/login.php");//link untuk login
       exit;
     }
       
   
 	include "connection/db.php";
-	$QuerySql = "SELECT *,sum(harga_obat*jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  year(tanggal_terjual)";
+	$QuerySql = "SELECT *,sum(harga_obat*jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  year(tanggal_terjual)";//fungsi untuk menampilkan jumlah obat berdsarkan tahun
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql);//untuk menghubungkan mysql  
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tampil Data Obat</title>
+	<title>Tampil Data Obat</title> 
 	<link rel="stylesheet" href="bulma.min.css">
 </head>
 <body>
