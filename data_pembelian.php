eaaeef9f361106abfc3e18d2f7712ed81e021528
Petani Kode <contoh@petanikode.com>
2019-04-25 08:49:08 +0700

    Tampil Data pembelian dari suplier & data penjualan

--- data_pembelian.php
+++ data_pembelian.php
@@ -1,17 +1,26 @@
-<!--AZ KHARISMA P-->
+<!--AZ KHARISMA PRATAMA-->
+<!--1700018272-->
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
 <?php
 	session_start();
 
-if (!isset($_SESSION["login1"])) {
+if (!isset($_SESSION["login1"])) {//untuk login sebelum function
     	  header("location: http://localhost/apotik-keuangan/login.php");
       exit;
     }
 	include "connection/db.php";
-	$QuerySql = "SELECT *,harga_beli*jumlah_obat AS total FROM `supplier`, `obat` WHERE supplier.kode_obat=obat.kode_obat";
+	$QuerySql = "SELECT *,harga_beli*jumlah_obat AS total FROM `supplier`, `obat` WHERE supplier.kode_obat=obat.kode_obat"; //untuk menampilkan jumlah harga beli obat
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql); //untuk menghubungkan dengan mysql databases
 ?> 
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html>
 <head>
 	<title>DATA PEMBELIAN</title>
@@ -21,17 +30,17 @@ if (!isset($_SESSION["login1"])) {
 <?php 
   include "navbar/navbar_pembelian.php";
  ?>
-<table class="table is-fullwidth" >
+<table class="table is-fullwidth" > <!--untuk menampilkan tabel data pembelian-->
   <thead>
     <tr>
-      <th scope="col"><a href="s_pembelian_supplier.php"> ID PEMBELIAN</a></th>
-      <th scope="col"><a href="s_pembelian_tanggal.php">TANGGAL PEMBELIAN</a></th>
-      <th scope="col"><a href="s_pembelian_ko.php">KODE OBAT</a></th>
-      <th scope="col"><a href="s_pembelian_nama.php">NAMA OBAT</a></th>
-      <th scope="col"><a href="s_pembelian_harga.php">HARGA BELI</a></th>
-      <th scope="col"><a href="s_pembelian_jumlah.php">JUMLAH PEMBELIAN</a></th>
-      <th scope="col"><a href="s_pembelian_total.php">HARGA TOTAL</a></th>
-      <th scope="col"><a href="s_pembelian_kadaluarsa.php">TANGGAL KADALUARSA</a></th>
+      <th scope="col"><a href="s_pembelian_supplier.php"> ID PEMBELIAN</a></th> <!--menampilkan Id supplay yang membeli -->
+      <th scope="col"><a href="s_pembelian_tanggal.php">TANGGAL PEMBELIAN</a></th> <!--menampilkan tanggal kapan obatnya di beli -->
+      <th scope="col"><a href="s_pembelian_ko.php">KODE OBAT</a></th><!--menampilkan kode obat yang dibeli-->
+      <th scope="col"><a href="s_pembelian_nama.php">NAMA OBAT</a></th><!--menampilkan nama obat yang di beli-->
+      <th scope="col"><a href="s_pembelian_harga.php">HARGA BELI</a></th><!--menampilkan harga yang di beli-->
+      <th scope="col"><a href="s_pembelian_jumlah.php">JUMLAH PEMBELIAN</a></th><!--menampilkan jumlah berapa obat yang dibeli -->
+      <th scope="col"><a href="s_pembelian_total.php">HARGA TOTAL</a></th><!--menampilkan harga keseluruhan yang di beli -->
+      <th scope="col"><a href="s_pembelian_kadaluarsa.php">TANGGAL KADALUARSA</a></th> <!-- -->
     </tr>
   </thead>
 		<?php
--- data_penjualan.php
+++ data_penjualan.php
@@ -1,17 +1,26 @@
-<!--AZ KHARISMA P-->
+<!--AZ KHARISMA PRATAMA-->
+<!--1700018272-->
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
 <?php
 	session_start();
 
-if (!isset($_SESSION["login1"])) {
+if (!isset($_SESSION["login1"])) {//untuk login sebelum function
     	  header("location: http://localhost/apotik-keuangan/login.php");
       exit;
     }
       
   
-	include "connection/db.php";
-	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat";
+	include "connection/db.php"; //panggilan database 
+	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat";//untuk menampilkan hatga jual obat
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql); //untuk menghubungkan mysql database
 ?> 
 <!DOCTYPE html>
 <html>
@@ -23,19 +32,20 @@ if (!isset($_SESSION["login1"])) {
 <?php 
   include "navbar/navbar_penjualan.php";
  ?>
-<table class="table is-fullwidth" >
+<table class="table is-fullwidth" ><!--tabel untuk menampilkan data penjualan obat -->
   <thead>
     <tr>
-      <th scope="col"><a href="s_penjualan_id.php"> ID PENJUALAN</a></th>
-      <th scope="col"><a href="s_penjualan_tanggal.php">TANGGAL TERJUAL</a></th>
-      <th scope="col"><a href="s_penjualan_ko.php">KODE OBAT</a></th>
-      <th scope="col"><a href="s_penjualan_nama.php">NAMA OBAT</a></th>
-      <th scope="col"><a href="s_penjualan_harga.php">HARGA OBAT</a></th>
-      <th scope="col"><a href="s_penjualan_jumlah.php">JUMLAH TERJUAL</a></th>
-      <th scope="col"><a href="s_penjualan_total.php">HARGA TOTAL</a></th>
+      <th scope="col"><a href="s_penjualan_id.php"> ID PENJUALAN</a></th><!--menampilkan Id data penjualan -->
+      <th scope="col"><a href="s_penjualan_tanggal.php">TANGGAL TERJUAL</a></th><!--menampilkan tanggal penjualan obat -->
+      <th scope="col"><a href="s_penjualan_ko.php">KODE OBAT</a></th><!--menampilkan kode obat untuk penjualan -->
+      <th scope="col"><a href="s_penjualan_nama.php">NAMA OBAT</a></th><!--menampilkan nama obat yang terjual -->
+      <th scope="col"><a href="s_penjualan_harga.php">HARGA OBAT</a></th><!--menampilkan nama obat yang terjual -->
+      <th scope="col"><a href="s_penjualan_jumlah.php">JUMLAH TERJUAL</a></th><!--menampilkan jumlah obat yang terjual -->
+      <th scope="col"><a href="s_penjualan_total.php">HARGA TOTAL</a></th><!--menampilkan harga total obat yang terjual -->
     </tr>
   </thead>
 		<?php
+		//panggilan data atribut di php
 			foreach ($SQL as $key) {
 				echo "<tr>
 						<td>$key[id_penjualan]</td>
