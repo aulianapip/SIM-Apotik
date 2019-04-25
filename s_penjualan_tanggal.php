0d099806d5918c8e742fb1dbd80326a464f7167f
aldidaemon <aldidaemom@gmail.com>
2019-04-25 08:46:01 +0700

    Sorting data penjualan by nama & tanggal

--- s_penjualan_tanggal.php
+++ s_penjualan_tanggal.php
@@ -1,4 +1,12 @@
 <!-- ALDIANSYAH DARMAWAN-->
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
 <?php
 	session_start();
 
@@ -8,8 +16,9 @@ if (!isset($_SESSION["login1"])) {
     }
       
   
-	include "connection/db.php";
-	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat ORDER BY tabel_penjualan.tanggal_terjual ASC ";
+	include "connection/db.php";	//Buat connect ke database
+	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat ORDER BY tabel_penjualan.tanggal_terjual ASC ";	//buat memanggil fungsi query untuk mengurutkan table penjualan berdasarkan tanggal terjual
+
 
 	$SQL = mysqli_query($connect, $QuerySql); 
 ?> 
@@ -17,13 +26,13 @@ if (!isset($_SESSION["login1"])) {
 <html>
 <head>
 	<title>Tampil Data Obat</title>
-	<link rel="stylesheet" href="bulma.min.css">
+	<link rel="stylesheet" href="bulma.min.css">	<!--untuk memanggil file css-->
 </head>
 <body>
 <?php 
-  include "navbar/navbar_penjualan.php";
+  include "navbar/navbar_penjualan.php"; //memanggil database navbar penjualan
  ?>
-<table class="table is-fullwidth" >
+<table class="table is-fullwidth" ><!--membuat table-->
   <thead>
     <tr>
       <th scope="col"><a href="s_penjualan_id.php"> ID PENJUALAN</a></th>
@@ -33,6 +42,7 @@ if (!isset($_SESSION["login1"])) {
       <th scope="col"><a href="s_penjualan_harga.php">HARGA OBAT</a></th>
       <th scope="col"><a href="s_penjualan_jumlah.php">JUMLAH TERJUAL</a></th>
       <th scope="col"><a href="s_penjualan_total.php">HARGA TOTAL</a></th>
+      <!--menampilkan navbar id penjualan,tanggal penjualan,kode obat,nama obat,harga obat,jumlah terjual,dan total-->
     </tr>
   </thead>
 		<?php
@@ -45,7 +55,7 @@ if (!isset($_SESSION["login1"])) {
 						<td>$key[harga_obat]</td>
 						<td>$key[jumlah_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";
+				</tr>"//buat menampilkan value id penjualan,tanggal penjualan,kode obat,nama obat,harga obat,jumlah terjual,dan total;
                 	
 				}
 		?>
