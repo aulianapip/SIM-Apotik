fb7a80a674358001a79a2a89e4dd7bdffa559283
fadhilabigail <fadhilabigailalvast@gmail.com>
2019-04-25 10:09:14 +0700

    Sorting penjualan berdasarkan harga

--- s_penjualan_harga.php
+++ s_penjualan_harga.php
@@ -1,6 +1,16 @@
 <!--FADHIL ABIGAIL-->
+
+<!--  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier
+-->
+
+
 <?php
-	session_start();
+	session_start(); // Untuk Memulai Eksekusi session pada server dan kemudian Menyimpannya pada browser dan posisinya 
+					 // HARUS PALING DEPAN.
 
 if (!isset($_SESSION["login1"])) {
     	  header("location: http://localhost/apotik-keuangan/login.php");
@@ -9,21 +19,22 @@ if (!isset($_SESSION["login1"])) {
       
   
 	include "connection/db.php";
-	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat ORDER BY obat.harga_obat ASC ";
+	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat ORDER BY obat.harga_obat ASC "; // Query ini berisi fungsi untuk melakukan Pensortingan 																	berdasarkan Harga Termurah di tabel Penjualan.  
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql); // variabel SQL menampung berupa perintah pada variabel SQL ke server yang berada di Variabel Connect //
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Tampil Data Obat</title>
-	<link rel="stylesheet" href="bulma.min.css">
+	<link rel="stylesheet" href="bulma.min.css"> <!-- Fungsi class ini digunakan untuk Pemanggilan CSS  -->
 </head>
 <body>
 <?php 
-  include "navbar/navbar_penjualan.php";
+  include "navbar/navbar_penjualan.php"; // Fungsi ini berguna untuk Pemanggilan Menu Navbar Menggunakan Pemanggilan lewaat php
+
  ?>
-<table class="table is-fullwidth" >
+<table class="table is-fullwidth" > <!-- Fungsi class ini digunakan untuk Pemanggilan CSS pada Fungsi Pembuatan Tabel -->
   <thead>
     <tr>
       <th scope="col"><a href="s_penjualan_id.php"> ID PENJUALAN</a></th>
@@ -45,7 +56,8 @@ if (!isset($_SESSION["login1"])) {
 						<td>$key[harga_obat]</td>
 						<td>$key[jumlah_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";
+					</tr>";
+                	//buat menampilkan value id penjualan,tanggal penjualan,kode obat,nama obat,harga obat,jumlah terjual,dan total;
                 	
 				}
 		?>
--- s_penjualan_id.php
+++ s_penjualan_id.php
@@ -1,6 +1,17 @@
 <!--FADHIL ABIGAIL-->
+
+
+<!--  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier
+-->
+
+
 <?php
-	session_start();
+	session_start(); // Untuk Memulai Eksekusi session pada server dan kemudian Menyimpannya pada browser dan posisinya 
+					 // HARUS PALING DEPAN.
 
 if (!isset($_SESSION["login1"])) {
     	  header("location: http://localhost/apotik-keuangan/login.php");
@@ -9,19 +20,20 @@ if (!isset($_SESSION["login1"])) {
       
   
 	include "connection/db.php";
-	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat ORDER BY tabel_penjualan.id_penjualan ASC ";
+	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat ORDER BY tabel_penjualan.id_penjualan ASC "; // Query ini berisi fungsi untuk melakukan 														Pensortingan berdasarkan Kode atau ID obat dari yang terkecil 													    hingga terbesar di tabel Penjualan. dengan relasi antara Entitas 													 tabel_penjualan.kode_obat=obat.kode_obat 
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql); // variabel SQL menampung berupa perintah pada variabel SQL ke server yang berada di Variabel Connect //
+
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Tampil Data Obat</title>
-	<link rel="stylesheet" href="bulma.min.css">
+	<link rel="stylesheet" href="bulma.min.css"> <!-- Fungsi class ini digunakan untuk Pemanggilan CSS  -->
 </head>
 <body>
 <?php 
-  include "navbar/navbar_penjualan.php";
+  include "navbar/navbar_penjualan.php"; // Fungsi ini berguna untuk Pemanggilan Menu Navbar Menggunakan Pemanggilan lewaat php
  ?>
 <table class="table is-fullwidth" >
   <thead>
