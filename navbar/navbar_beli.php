1adab1683935d772ff009c4141709603a5ae5681
zakikii <alwanzakii@gmail.com>
2019-04-25 09:57:41 +0700

    Navbar Beli

--- navbar/navbar_beli.php
+++ navbar/navbar_beli.php
@@ -1,14 +1,22 @@
-<!--ALWAN ZAKI-->
+<!--ALWAN ZAKI 1700018259-->
+<!--
+Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+-->
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="edit.css">
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-  <script type="text/javascript" src="js/jquery.js"></script>
-  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <link rel="stylesheet" type="text/css" href="edit.css"><!-- menggunakan css dari edit.css --> 
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css"><!-- menggunakan tipe css/bootstrap --> 
+  <script type="text/javascript" src="js/jquery.js"></script><!-- menggunakan script dari js/jquery.js -->
+  <script type="text/javascript" src="js/bootstrap.js"></script><!-- menggunakan script dari js/bootstrap.js -->
 </head>
 <body>
-<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+<nav class="navbar is-success" role="navigation" aria-label="main navigation"><!--menggunakan kelas navbar is-success-->
   <div class="navbar-brand">
    
 
@@ -21,11 +29,11 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item" href="home.php">
+      <a class="navbar-item" href="home.php"><!-- memanggil home.php" -->
        Kembali
       </a>
      <a class="navbar-item" href="http://localhost/apotik-keuangan/Administration/data_pembelian.php">
-       Cek obat yang telah dibeli
+      <!-- memanggil http://localhost/apotik-keuangan/Administration/data_pembelian.php" -->
       </a>
       
   </div>
