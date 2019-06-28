38a307dfd98451ee71654cb9879f82d4df371a46
fadhilabigail <fadhilabigailalvast@gmail.com>
2019-06-29 04:09:05 +0700

    Update Data Opname

--- dataopname.php
+++ dataopname.php
@@ -52,16 +52,18 @@
     
   </div>
 
+
+
 </nav>
 <table class="table is-fullwidth" >
   <thead>
     <tr>
-      <th scope="col">Kode Obat</th>
       <th scope="col">Kode Barcode</th>
+      <th scope="col">Kode Kode Obat</th>
       <th scope="col">Status</th>
-      <th scope="col">Catatan</th>
       <th scope="col">Tanggal</th>
-      <th scope="col">ACTION</th>
+      <th scope="col">Catatan</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
 		<?php
@@ -70,11 +72,11 @@
       $query = mysqli_query($connect,"SELECT barcode.kode_obat AS kode_obat,opname.kode_barcode AS kode_barcode,opname.status AS status,catatan,tanggal from opname INNER JOIN barcode where opname.kode_barcode = barcode.kode_barcode ORDER BY nomor_pasok ASC");
 			foreach ($query as $data) {
 				echo "<tr>
-                <td>$data[kode_obat]</td>
                 <td>$data[kode_barcode]</td>
+                <td>$data[kode_obat]</td>
                 <td>$data[status]</td>
-                <td>$data[catatan]</td>
                 <td>$data[tanggal]</td>
+                <td>$data[catatan]</td>
                 <td>
                 <a href='delete_opname.php?kode_barcode=$data[kode_barcode]'>DELETE</a>
             </td>
