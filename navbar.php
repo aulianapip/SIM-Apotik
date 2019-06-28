8acf1261ad732cf0c5cc29e4ea46fb4088a82f8c
fesus19 <febrisuseno62@gmail.com>
2019-06-29 04:15:00 +0700

    Navbar

---
+++ navbar.php
@@ -0,0 +1,47 @@
+<nav class="navbar is-success" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+   
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="login.html">
+    <img src="logut.png"></img>
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          Daftar Tabel
+        </a>
+
+        <div class="navbar-dropdown">
+          
+          <a class="navbar-item" href="dataobat.php">
+            Obat
+          </a>
+          <a class="navbar-item" href="datasupiler.php">
+            Supplier
+          </a>
+          <a class="navbar-item" href="dataopname.php">
+            Opname
+          </a>
+    
+        </div>
+      </div>
+
+  	  <a class="navbar-item" href="tambah_opname.php">
+       Tambah Opname
+      </a>
+  </div>
+</div>
+    </div>
+
+    
+  </div>
+</nav>
