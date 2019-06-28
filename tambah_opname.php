a690eefb97e985c3453395db9ff2dfa9648cdebc
rekarachmadi21 <rekarachmadi23@gmail.com>
2019-06-28 23:53:41 +0700

    update tambah opname

--- tambah_opname.php
+++ tambah_opname.php
@@ -6,7 +6,37 @@
 <body>
   <?php 
     include "db.php";
-    if (isset($_POST['cek'])) {
+    if (isset($_POST['tambah'])) {
+      //TAMPIL DI FORM INPUT
+      $query = "SELECT kode_obat from obat ";
+      $cekobat = mysqli_query($connect,$query);
+      $barcode = "SELECT kode_barcode from barcode ORDER BY nomor_pasok ASC";
+      $cekbarcode = mysqli_query($connect,$barcode);
+      //AMBIL DATA DI FORM INPUT
+      $kode_barcode = $_POST['kode_barcode'];
+      $status = $_POST['status'];
+      $catatan = $_POST['catatan'];
+      //Tambah Opname
+      $tambah_opname = "INSERT INTO opname VALUES ('$kode_barcode','$status','$catatan',curdate())";
+      mysqli_query($connect,$tambah_opname);
+      //Update Barcode
+      $update_barcode = "UPDATE barcode SET status='$status' WHERE kode_barcode='$kode_barcode'";
+      mysqli_query($connect,$update_barcode);
+      //Update Pasok
+      $barcode1 = "SELECT kode_pasok FROM barcode WHERE kode_barcode='$kode_barcode'";
+      $barcode2 = mysqli_query($connect,$barcode1);
+      $bar1 = mysqli_fetch_array($barcode2);
+      $kode_pasok = $bar1['kode_pasok'];
+      $pas1 = "SELECT jumlah_pasok FROM pasok where kode_pasok='$kode_pasok'";
+      $pas2 = mysqli_query($connect,$pas1);
+      $hehe = mysqli_fetch_array($pas2);
+      if ($status != "DI GUDANG") {
+        $jumlah_pasok= $hehe['jumlah_pasok']-1;
+        $update_p = "UPDATE pasok SET jumlah_pasok='$jumlah_pasok' WHERE kode_pasok='$kode_pasok'";
+        mysqli_query($connect,$update_p);
+      }
+       header("location: dataopname.php");
+    }elseif (isset($_POST['cek'])) {
       $query = "SELECT kode_obat from obat ";
       $cekobat = mysqli_query($connect,$query);
       $barcode = "SELECT kode_barcode from barcode ORDER BY nomor_pasok ASC";
