d39ccbecdf790b48285e1fcbd9e07e198f989895
amandadyn07 <fahmidyna25@gmail.com>
2019-06-29 07:42:35 +0700

    hapus

--- database/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username = "root";
-$database = "cf";
-$password = "";
-$server = "localhost";
-
-$connect = mysqli_connect($server, $username, $password, $database) or die("Koneksi bermasalah");
