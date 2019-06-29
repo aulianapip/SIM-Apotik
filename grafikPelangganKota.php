26ac08c3682eaa3df478c3c674620df350e74a00
YogaFirzaSabbihisma <yogazah@gmail.com>
2019-06-29 16:41:42 +0700

    Memperbaiki File Untuk Menampilkan Grafik

--- grafikPelangganKota.php
+++ grafikPelangganKota.php
@@ -22,7 +22,7 @@
 
 
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "crm");
+$koneksi = mysqli_connect("localhost", "root", "", "sim-apotek-fix");
 ?>
 
 	<div style="width: 800px;margin: 0px auto;">
