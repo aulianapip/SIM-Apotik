a911b4fff6e57c5a6996dd1bb73a0a22c0971964
aldidaemon <aldidaemom@gmail.com>
2019-07-02 16:15:02 +0700

    Update data pembelian hari

--- pembelian_hari.php
+++ pembelian_hari.php
@@ -1,8 +1,8 @@
-<!--AIRLA ISMAIL -->
 <!--
   @changelog:
     Amanda Fahmidyna v 1.0.2 (Memperbaiki fitur)
-    Airla Ismail v 1.0.1 (Membuat fitur)
+    Aldiansyah Darmawan v 1.0.1 (Menambah fitur)
+    Airla ismail (Mebuat fitur)
 -->
 <?php
   session_start();
@@ -11,33 +11,40 @@ if (!isset($_SESSION["login1"])) {
         header("location: http://localhost/Keuangan/login.php");
       exit;
     }
+    
   require_once ('database/deb.php');
-  $QuerySql = "SELECT *, tanggal,sum(jumlah) as jumlah_pasok,sum(harga*jumlah) AS total FROM jualbeli group by day(tanggal) ASC";
+  $QuerySql = "SELECT *, tanggal , SUM(jumlah) as jumlah_pasok,SUM(harga*jumlah) AS total FROM jualbeli group by day(tanggal) ASC";
 
   $SQL = mysqli_query($connect, $QuerySql); 
  ?> 
+
 <!DOCTYPE html>
 <html>  
 <head>
-  <title>DATA PEMBELIAN</title>
+  <title>DATA PEMBELIAN </title>
   <link rel="stylesheet" href="bulma.min.css">
 </head>
 <body>
+
 <?php 
   include "navbar/navbar_pembelian.php";
  ?>
+
 <table class="table is-fullwidth" >
+  
   <thead>
     <tr>
-      <th scope="col"><a href="s_pembelian_tanggal.php">WAKTU PEMBELIAN</a></th>
-      <th scope="col"><a href="s_pembelian_harga.php">JUMLAH BELI</a></th>
- 
-      <th scope="col"><a href="s_pembelian_total.php">HARGA TOTAL</a></th>
+      <th scope="col" ><a href="s_pembelian_tanggal.php"> WAKTU PEMBELIAN</a></th>
+      <th scope="col" ><a href="s_pembelian_harga.php">   JUMLAH BELI</a></th>
+      <th scope="col" ><a href="s_pembelian_total.php">   HARGA TOTAL</a></th>
+    
     </tr>
   </thead>
+
     <?php
       foreach ($SQL as $key) {
-        echo "<tr>
+        echo "
+        <tr>
             <td>$key[tanggal]</td>
             <td>$key[jumlah_pasok]</td>
             <td>$key[total]</td>
