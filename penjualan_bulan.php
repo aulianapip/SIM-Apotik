7415be1cee4f56a4ae84fe90d364ed132d73ad9e
Fadlioei22 <Fadlioei06@gmail.com>
2019-07-23 20:31:47 +0700

    update by Fadli Oei

--- penjualan_bulan.php
+++ penjualan_bulan.php
@@ -1,22 +1,30 @@
-<!-- THOBIE ZATONI -->
+<!--Fadli muhammad oei-->
+<!--1600018057-->
+<!-- Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+   -->
 <?php
 	session_start();
 
 if (!isset($_SESSION["login1"])) {
-    	  header("location: http://localhost/apotik-keuangan/login.php");
+    	  header("location: http://localhost/apotik-keuangan/login.php"); // pemanggilan login.php
       exit;
     }
       
   
-	include "connection/db.php";
-	$QuerySql = "SELECT *,sum(jumlah_terjual) as jumlah_terjual,sum(harga_obat) as harga_obat,sum(harga_obat)*sum(jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  month(tanggal_terjual)";
+	include "connection/db.php"; // untuk konek ke database
+	$QuerySql = "SELECT *,sum(jumlah_terjual) as jumlah_terjual,sum(harga_obat) as harga_obat,sum(harga_obat)*sum(jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  month(tanggal_terjual)"; // untuk mengambil data penjualan, jumlah terjual, harga obat, jumlah harga obat, total total pemasukan dari tabel penjualan dan tabel obat berdasarkan bulan
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql);  //menghubungkan ke database, serta mengambil data berdasarkan query
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tampil Data Obat</title>
+	<title>Tampil Data Obat</title> <!-- title bar atau judul -->
 	<link rel="stylesheet" href="bulma.min.css">
 </head>
 <body>
@@ -31,7 +39,7 @@ if (!isset($_SESSION["login1"])) {
       <th scope="col">JUMLAH TERJUAL</th>
       <th scope="col">TOTAL PENJUALAN</th>
     </tr>
-  </thead>
+  </thead> <!-- head tabel -->  
 		<?php
 			foreach ($SQL as $key) {
 				echo "<tr>
@@ -39,7 +47,7 @@ if (!isset($_SESSION["login1"])) {
 						<td>$key[harga_obat]</td>
 						<td>$key[jumlah_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";
+				</tr>"; // menampilkan data (tanggal terjual, harga obat, jumlah terjual, total) 
                 	
 				}
 		?>
