9f1786a5e932c7ff73afb75a75e58a573d9615af
Fadlioei22 <Fadlioei06@gmail.com>
2019-07-23 20:34:37 +0700

    update

--- penjualan_minggu.php
+++ penjualan_minggu.php
@@ -1,5 +1,5 @@
-<!--FEBRI SUSENO-->
-<!--1600018078-->
+<!--Fadli muhammad oei-->
+<!--1600018057-->
 <!-- Penjelasan class :
   Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
   data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
@@ -10,21 +10,21 @@
 <?php
 	session_start();// untuk melakukan aktifitas yang berhubungan dengan interaksi user pada sebuah web server php.
 
-if (!isset($_SESSION["login1"])) { //jika login gagal maka kembali ke login.php
-    	  header("location: http://localhost/apotik-keuangan/login.php");//link untuk login
+if (!isset($_SESSION["login1"])) { // status login menggunakan session, jika login gagal maka kembali ke login.php
+    	  header("location: http://localhost/apotik-keuangan/login.php"); //pemanggilan login.php nya
       exit;
     }
       
   
 	include "connection/db.php";
-	$QuerySql = "SELECT *,sum(harga_obat) as harga_obat,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  week(tanggal_terjual)";//fungsi untuk menampilkan jumlah obat berdsarkan minggu
+	$QuerySql = "SELECT *,sum(harga_obat) as harga_obat,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  week(tanggal_terjual)"; //fungsi untuk mengambil jumlah harga obat, harga obat, jumlah terjual, total dari tabel penjualan berdasarkan minggu
 
-	$SQL = mysqli_query($connect, $QuerySql); //untuk menghubungkan mysql
+	$SQL = mysqli_query($connect, $QuerySql); //menghubungkan ke database, serta mengambil data berdasarkan query
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tampil Data Obat</title>
+	<title>Tampil Data Obat</title> <!-- title bar -->
 	<link rel="stylesheet" href="bulma.min.css">
 </head>
 <body>
@@ -39,7 +39,7 @@ if (!isset($_SESSION["login1"])) { //jika login gagal maka kembali ke login.php
       <th scope="col">JUMLAH TERJUAL</th>
       <th scope="col">TOTAL PENJUALAN</th>
     </tr>
-  </thead>
+  </thead> <!-- head tabel dari = tanggal, harga obat, jumlah terjual, total penjualan -->
 		<?php
 			foreach ($SQL as $key) {
 				echo "<tr>
@@ -47,7 +47,7 @@ if (!isset($_SESSION["login1"])) { //jika login gagal maka kembali ke login.php
 						<td>$key[harga_obat]</td>
 						<td>$key[jumlah_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";//untuk menampilkan isi dari atribut di dalam tabel
+				</tr>";//untuk menampilkan isi dari atribut di dalam tabel yaitu (tanggal terjual, harga obat, jumlah terjual, total)
                 	
 				}
 		?>
