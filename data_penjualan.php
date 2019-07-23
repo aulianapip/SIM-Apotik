c5b1a9c0940251e1e3499227acb8b46ba4d331b5
Fadlioei22 <Fadlioei06@gmail.com>
2019-07-23 20:24:12 +0700

    update by fadli oei

--- data_penjualan.php
+++ data_penjualan.php
@@ -1,31 +1,23 @@
-<!--AZ KHARISMA PRATAMA-->
-<!--1700018272-->
-<!--
-Penjelasan class :
-  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
-  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
-  data pembelian hanya menampilkan data pembelian barang dari suplier. 
-  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
-  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
--->
+<!--Fadli muhammad oei-->
+<!--1600018057-->
 <?php
 	session_start();
 
-if (!isset($_SESSION["login1"])) {//untuk login sebelum function
+if (!isset($_SESSION["login1"])) { // status login menggunakan session, jika login gagal maka kembali ke login.php
     	  header("location: http://localhost/apotik-keuangan/login.php");
       exit;
     }
       
   
-	include "connection/db.php"; //panggilan database 
-	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat";//untuk menampilkan harga jual obat
+	include "connection/db.php"; // mengkonek kan ke database
+	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat"; //fungsi untuk mengambil harga obat, jumlah terjual, total dari tabel penjualan
 
-	$SQL = mysqli_query($connect, $QuerySql); //untuk menghubungkan mysql database
+	$SQL = mysqli_query($connect, $QuerySql); //menghubungkan ke database, serta mengambil data berdasarkan query
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tampil Data Obat</title>
+	<title>Tampil Data Obat</title> <!-- title bar -->
 	<link rel="stylesheet" href="bulma.min.css">
 </head>
 <body>
@@ -55,7 +47,7 @@ if (!isset($_SESSION["login1"])) {//untuk login sebelum function
 						<td>$key[harga_obat]</td>
 						<td>$key[jumlah_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";
+				</tr>"; // menampilkan data (id penjualan, tanggal terjual, kode obat, nama obat, harga obat, jumlah terjual, total)
                 	
 				}
 		?>
