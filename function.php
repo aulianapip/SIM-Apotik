a8226b4a685f19692438560b9c74f839cee8c778
adeliazakiyyah29 <adeliafitriawati29@gmail.com>
2019-07-23 21:35:47 +0700

    membuat function riwayat di file function.php

--- function.php
+++ function.php
@@ -1,6 +1,5 @@
 <?php 
 	//di index.php
-	//dikerjakan carto ardiyanto 1700018283
 	function tampil_berdasarkan_tanggal(){
 	$q1="SELECT * from pelanggan where tgl_daftar between '$tgl_awal' and '$tgl_akhir'";
 		$SQL=mysqli_query($connect,$q1);
@@ -26,7 +25,7 @@
 	}
 	//hapus.php
 	function hapus(){
-
+		
 		return 
 	}
 	//deleteselect.php
@@ -40,8 +39,10 @@
 		return 
 	}
 
-	function riwayat_satu(){
-
+	function riwayat_satu(){//Adelia fitriawati z 1700018281
+	include "connect.php" //untuk menyambungkan ke database yang sudah dibuat dengan nama folder 
+	$q1="SELECT penjualan.tgl_penjualan AS waktu,penjualan.tipe as tipe,penjualan.id as id,pelanggan.Nama as nama,penjualan.no_transaksi as no_transaksi,penjualan_detail.kd_barang as kode_obat,penjualan_detail.jumlah as jumlah_beli, penjualan_detail.sub_total as total_beli from penjualan,pelanggan,penjualan_detail where penjualan.id=pelanggan.ID and penjualan.tipe=pelanggan.tipe and penjualan.tipe=penjualan_detail.tipe and penjualan.id=penjualan_detail.id and penjualan.no_transaksi=penjualan_detail.no_transaksi and penjualan.tgl_penjualan between '$tgl_awal' and '$tgl_akhir'"; //query function untuk melihat daftar pembelian member.
+  	$SQL=mysqli_query($connect,$q1); //query ini berfungsi untuk memanggil fungsi conect untuk memberikan akses ke database dan inisialisasi dari querysql
 		return
 	}
 
