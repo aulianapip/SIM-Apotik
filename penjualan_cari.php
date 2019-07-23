7f09fc6eb4f4f44f576b966df8fc7cca09490e19
Fadlioei22 <Fadlioei06@gmail.com>
2019-07-23 20:32:50 +0700

    update

--- penjualan_cari.php
+++ penjualan_cari.php
@@ -1,23 +1,32 @@
-<!--LUSSY IKA--><?php
+<!--Fadli muhammad oei-->
+<!--1600018057-->
+<!-- Penjelasan class :
+  Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
+  data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
+  data pembelian hanya menampilkan data pembelian barang dari suplier. 
+  data penjualan gambaran informasi data-data penjualan yang dihasilkan dari penjualan kasir.
+  total keuntungan menampilkan keuntungan dari harga jual tiap barang dikurangi harga beli dari suplier.
+   -->
+<?php
 	session_start();
 
-if (!isset($_SESSION["login1"])) {
-    	  header("location: http://localhost/apotik-keuangan/login.php");
+if (!isset($_SESSION["login1"])) { // status login menggunakan session
+    	  header("location: http://localhost/apotik-keuangan/login.php"); //pemanggilan login.php nya
       exit;
     }
       
   
-	include "connection/db.php";
+	include "connection/db.php"; // mengkonek kan ke database
 
 	$cari = $_POST['cari'];
-	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat AND obat.nama_obat LIKE '%$cari%'";
+	$QuerySql = "SELECT *,harga_obat*jumlah_terjual as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat AND obat.nama_obat LIKE '%$cari%'"; //untuk mengambil data harga obat,jumlah terjual, total, tabel penjualan, serta obat
 
-	$SQL = mysqli_query($connect, $QuerySql); 
+	$SQL = mysqli_query($connect, $QuerySql); //menghubungkan ke database, serta mengambil data berdasarkan query
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tampil Data Obat</title>
+	<title>Tampil Data Obat</title> <!-- title bar -->
 	<link rel="stylesheet" href="bulma.min.css">
 </head>
 <body>
@@ -35,7 +44,7 @@ if (!isset($_SESSION["login1"])) {
       <th scope="col"><a href="s_penjualan_jumlah.php">JUMLAH TERJUAL</a></th>
       <th scope="col"><a href="s_penjualan_total.php">HARGA TOTAL</a></th>
     </tr>
-  </thead>
+  </thead> <!-- head tabel dari = id penjualan, tanggal terjual, kode obat, nama obat, harga obat, jumlah terjual, harga total -->
 		<?php
 			foreach ($SQL as $key) {
 				echo "<tr>
@@ -46,7 +55,7 @@ if (!isset($_SESSION["login1"])) {
 						<td>$key[harga_obat]</td>
 						<td>$key[jumlah_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";
+				</tr>"; // menampilkan data (id penjualan, tanggal terjual, kode obat, nama obat, harga obat, jumlah terjual, total)
                 	
 				}
 		?>
