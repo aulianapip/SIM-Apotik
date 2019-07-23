1dae79d89b14d1e02e2f815d33b8bd38f22b26c6
Fadlioei22 <Fadlioei06@gmail.com>
2019-07-23 20:35:27 +0700

    update

--- penjualan_tahun.php
+++ penjualan_tahun.php
@@ -1,5 +1,5 @@
-<!--FEBRI SUSENO-->
-<!--1600018078-->
+<!--Fadli muhammad oei-->
+<!--1600018057-->
 <!-- Penjelasan class :
   Dalam keuangan kami membuat beberapa function seperti cashflow, data pembelian,
   data penjualan, dan total keuntungan. cashflow gambaran mengenai jumlah uang yang masuk dan keluar. 
@@ -10,21 +10,21 @@
 <?php
 	session_start();// untuk melakukan aktifitas yang berhubungan dengan interaksi user pada sebuah web server php.
 
-if (!isset($_SESSION["login1"])) { //jika login gagal maka kembali ke login.php
-    	  header("location: http://localhost/apotik-keuangan/login.php");//link untuk login
+if (!isset($_SESSION["login1"])) { // status login menggunakan session, jika login gagal maka kembali ke login.php
+    	  header("location: http://localhost/apotik-keuangan/login.php"); //pemanggilan login.php nya
       exit;
     }
       
   
 	include "connection/db.php";
-	$QuerySql = "SELECT *,sum(harga_obat*jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  year(tanggal_terjual)";//fungsi untuk menampilkan jumlah obat berdsarkan tahun
+	$QuerySql = "SELECT *,sum(harga_obat*jumlah_terjual) as total FROM `tabel_penjualan`, `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat GROUP BY  year(tanggal_terjual)";//fungsi untuk mengambil jumlah obat berdasarkan tahun
 
-	$SQL = mysqli_query($connect, $QuerySql);//untuk menghubungkan mysql  
+	$SQL = mysqli_query($connect, $QuerySql); //menghubungkan ke database, serta mengambil data berdasarkan query
 ?> 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tampil Data Obat</title> 
+	<title>Tampil Data Obat</title> <!-- title bar -->
 	<link rel="stylesheet" href="bulma.min.css">
 </head>
 <body>
@@ -37,13 +37,13 @@ if (!isset($_SESSION["login1"])) { //jika login gagal maka kembali ke login.php
       <th scope="col">TANGGAL</th>
       <th scope="col">TOTAL PENJUALAN</th>
     </tr>
-  </thead>
+  </thead> <!-- head tabel dari = tanggal, total penjualan -->
 		<?php
 			foreach ($SQL as $key) {
 				echo "<tr>
 						<td>$key[tanggal_terjual]</td>
 						<td>$key[total]</td>
-				</tr>";
+				</tr>"; //untuk menampilkan isi dari atribut di dalam tabel yaitu (tanggal terjual, total)
                 	
 				}
 		?>
