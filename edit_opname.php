9df1f6490ccbd356dba988d9523aec5e905ff301
rekarachmadi21 <rekarachmadi23@gmail.com>
2019-07-25 01:12:36 +0700

    UAS (coment) UNTUK MEGEDIT DATA YANG ADA PADA OPNAME - 1700018237

--- edit_opname.php
+++ edit_opname.php
@@ -1,4 +1,9 @@
+<!-- Reka Rachmadi 1700018237 !-->
+<!--uas no 1. Stock Opname adalah kegiatan perhitungan secara fisik atas persediaan barang di gudang 
+secara fisik atas persedian barang di gudang yang akan dijual.Pada fitur yang kami buat
+kami menginputkan status kondisi barang yang berada ditoko, status antara lain adalah digudang rusak, hilang ,di pinjam , dan terjual. jika terjadi kesalahan input status maka dapat diubah dengan fitur edit, dan bila barang telah kembali atau di ganti atau telah di konfirmasi oleh pihak gudang dan kasir maka data opname dapat di hapus dengan fitur delete  -->
 <!DOCTYPE html>
+
 <html>
 <head>
   <title>Edit Opname</title>
@@ -7,13 +12,13 @@
   <?php 
        
     if (isset($_POST['tambah'])) {
-      include 'db.php';
+      include 'db.php';//koneksi database
       $kode_barcode = $_POST['kode_barcode']; 
       $status = $_POST['status'];
       $catatan = $_POST['catatan'];
-      $query = "UPDATE opname SET status='$status',catatan='$catatan' WHERE kode_barcode='$kode_barcode'";
+      $query = "UPDATE opname SET status='$status',catatan='$catatan' WHERE kode_barcode='$kode_barcode'";//update status opname dari form input 
       mysqli_query($connect, $query);
-      header("location: dataopname.php"); 
+      header("location: dataopname.php");//di update ke file dataopname.php
     }else{
 
     }
