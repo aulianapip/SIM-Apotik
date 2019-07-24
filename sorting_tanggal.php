bf4aebf43a1b9381b862ddf2d939dcff1a14b345
fesus19 <febri1600018078@webmail.uad.ac.id>
2019-07-24 22:45:23 +0700

    UAS-Menampilkan Data Pada Opname shorting berdasarkan tanggal - 1600018078

--- sorting_tanggal.php
+++ sorting_tanggal.php
@@ -1,4 +1,7 @@
 <!-- febrisuseno.1600018078 -->
+<!--no1. Stock Opname adalah kegiatan perhitungan secara fisik atas persediaan barang di gudang 
+secara fisik atas persedian barang di gudang yang akan dijual.Pada fitur yang kami buat
+kami menginputkan status kondisi barang yang berada ditoko, status antara lain adalah digudang rusak, hilang ,di pinjam , dan terjual. jika terjadi kesalahan input status maka dapat diubah dengan fitur edit, dan bila barang telah kembali atau di ganti atau telah di konfirmasi oleh pihak gudang dan kasir maka data opname dapat di hapus dengan fitur delete  -->
 <!DOCTYPE html>
 <html>
 <head>
