acdec450a7d54b8b23feb08d549422736fb5c4c2
hernism29 <hernism7@gmail.com>
2019-07-24 22:09:46 +0700

    Menambahkan function hapus dan hapus ceklist

--- function.php
+++ function.php
@@ -26,12 +26,31 @@
 	//hapus.php
 	function hapus(){
 		
+		
+	include 'conect.php'; //untuk menyambungkan ke database yang sudah dibuat dengan nama folder conect.php
+	$ID = $_GET['ID']; //untuk mendapatkan id pada database yang sudah ada di dalamnya
+	$QuerySql = "DELETE FROM pelanggan WHERE ID='$ID' "; //sintaks penghapusan data secara langsung dengan tombol delete
+	$SQL = mysqli_query($connect,$QuerySql);//untul menjalankan sintaks diatas agar sinkron dengan database
+	header("location:index.php?pesan=Hapus");//ketika data sudah terhapus, maka tampilan akan merefresh ke dalam tampilan awal yaitu index
+		
 		return 
 	}
 	//deleteselect.php
 	function hapus_ceklis(){
 
-		return
+		include 'conect.php'; //untuk menyambungkan ke database yang sudah dibuat dengan nama folder conect.php
+	$ID = $_POST['pilih']; //untuk memanggil saat multiple delete dipilih dengan opsi, mana saja data yang akan di delete
+	$jumlah_dipilih = count($ID); //dan ketika opsi telah di panggil, maka akan di proses dengan perulangan sesuai dengan opsi yang kita pilih
+
+	for($x=0; $x<$jumlah_dipilih; $x++){
+
+	mysqli_query($connect,"DELETE FROM pelanggan WHERE ID ='$ID[$x]'"); //disini, kodingan akan memproses dalam mendelete data yang telah kita pilih untuk didelete
+ 
+}
+ 
+header("location:selectdelete.php"); // setelah berhasil di delete, maka akan kembali atau refresh ke folder awal untul mendelete data.
+
+		return ;
 	}
 
 	function input(){
@@ -48,5 +67,7 @@
 
 	function riwayat_dua(){
 		$SQL = mysqli_query($connect, "SELECT penjualan.tgl_penjualan AS waktu,penjualan.tipe as tipe,penjualan.id as id,pelanggan.Nama as nama, penjualan.no_transaksi as no_transaksi,penjualan_detail.kd_barang as kode_obat,penjualan_detail.jumlah as jumlah_beli, penjualan_detail.sub_total as total_beli from penjualan,pelanggan,penjualan_detail where penjualan.id=pelanggan.ID and penjualan.tipe=pelanggan.tipe and penjualan.tipe=penjualan_detail.tipe and penjualan.id=penjualan_detail.id and penjualan.no_transaksi=penjualan_detail.no_transaksi;"); //query function untuk melihat daftar pembelian member.
+		return
 	} //sampe line yang di sini
+
  ?>
