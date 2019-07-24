9c7c05139f1cc78eb2aa7089d1bb32f26b1245b5
zakikii <alwanzakii@gmail.com>
2019-07-25 01:30:33 +0700

    UAS (coment) KONEKSI KE DATABASE

--- db.php
+++ db.php
@@ -1,6 +1,9 @@
-<!--Aditya Gusti Mandala Putra-->
+<!--Alwan Zaki 1700018259-->
+<!--uas no 1. Stock Opname adalah kegiatan perhitungan secara fisik atas persediaan barang di gudang 
+secara fisik atas persedian barang di gudang yang akan dijual.Pada fitur yang kami buat
+kami menginputkan status kondisi barang yang berada ditoko, status antara lain adalah digudang rusak, hilang ,di pinjam , dan terjual. jika terjadi kesalahan input status maka dapat diubah dengan fitur edit, dan bila barang telah kembali atau di ganti atau telah di konfirmasi oleh pihak gudang dan kasir maka data opname dapat di hapus dengan fitur delete  -->
 <?php
 
-	$connect = new mysqli("localhost", "root", "", "sim-apotik");
+	$connect = new mysqli("localhost", "root", "", "sim-apotik");//queri untuk konek ke database
 
 ?>
