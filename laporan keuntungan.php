73025238d95f6ce2b9ee18774ef492fe6007170f
vikri2607@gmail.com <vikri2607@gmail.com>
2019-07-24 20:11:37 +0700

    Menambah Keterangan

--- laporan keuntungan.php
+++ laporan keuntungan.php
@@ -4,17 +4,17 @@
 	
  	 -->
  <?php 
-require_once('database/deb.php');
-include "navbar_cashflow.php";
+require_once('database/deb.php'); //mengonecctkan ke data base
+include "navbar_cashflow.php"; //ditampilkan dalam file navbar_cashflow.php
 
 
 
 // Vikri Ammar Kholis 1700018243
-$q = "SELECT sum(total_penjualan) as masuk from penjualan";
+$q = "SELECT sum(total_penjualan) as masuk from penjualan"; //menjumlah semua total penjualan sebagai masuk dari tabel penjualan
 $sql = mysqli_query($connect,$q);
-$q = "SELECT sum(jualbeli.jumlah*jualbeli.harga) AS keluar  FROM jualbeli WHERE jualbeli.jenis= 'kredit'"; 
+$q = "SELECT sum(jualbeli.jumlah*jualbeli.harga) AS keluar  FROM jualbeli WHERE jualbeli.jenis= 'kredit'"; //query menjumlah atribut jumlah dari tabel jual beli dikali kan jumah dari atribut harga di dalam tebel jual beli sebagai Keluar dari tabel jual beli di sortir berdasarkan jenis dengan pilihan  kredit dalam tebel jual beli
 $sql1 = mysqli_query($connect,$q);
-$q = "SELECT sum(jualbeli.jumlah*jualbeli.harga) AS Gaji  FROM jualbeli WHERE jualbeli.lainnya= 'Gaji Karyawan'";
+$q = "SELECT sum(jualbeli.jumlah*jualbeli.harga) AS Gaji  FROM jualbeli WHERE jualbeli.lainnya= 'Gaji Karyawan'";//query menjumlah atribut jumlah dari tabel jual beli dikali kan jumah dari atribut harga di dalam tebel jual beli sebagai GAJI dari tabel jual beli di sortir berdasarkan lainnya dengan pilihan Gajih Karyawan dalam tebel jual beli
 $sql2 = mysqli_query($connect,$q);
  ?>
 
@@ -27,7 +27,7 @@ $sql2 = mysqli_query($connect,$q);
 
  <!-- Bootstrap CSS -->
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
- <link rel="stylesheet" href="bulma.min.css">
+ <link rel="stylesheet" href="bulma.min.css"> <!-- untuk connect ke CSS -->
 
 	<title>laba</title>
 </head>
@@ -35,45 +35,45 @@ $sql2 = mysqli_query($connect,$q);
 
 <body>
 
-	<table class="table">
+	<table class="table"> <!--untuk membuat tampilan berupa tabel -->
 <thead>
  <!--tabel cashflow-->
-  <th>Pemasukan kotor</th>
+  <th>Pemasukan kotor</th> <!--isi tabel-->
 
- <th>Gaji Karyawan</th>
+ <th>Gaji Karyawan</th> <!--isi tabel-->
 
- <th>Pengeluaran</th>
+ <th>Pengeluaran</th> <!--isi tabel-->
 
- <th>Keuntungan</th>
+ <th>Keuntungan</th> <!--isi tabel-->
 
  </thead>
 
 
 <?php
- foreach ($sql as $data) {}
+ foreach ($sql as $data) {} // untuk mengonect kan query
 
- foreach ($sql1 as $data1) {}
+ foreach ($sql1 as $data1) {} // untuk mengonect kan query
 
- foreach ($sql2 as $data2) {}
+ foreach ($sql2 as $data2) {} // untuk mengonect kan query
  ?>
- <tr class = "table-primary">
+ <tr class = "table-primary"> <!--untuk membuat tampilan tabel berwarna-->
  <td>
  <?php
- echo "Rp ". $data['masuk'];
+ echo "Rp ". $data['masuk']; // untuk mengonect kan query sebagai masuk
  ?>
  </td>
  <td>
  <?php
- echo "Rp ". $data2['Gaji'];
+ echo "Rp ". $data2['Gaji']; // untuk mengonect kan query sebagai gaji
  ?>
  </td>
  <td>
- <?php echo "Rp ". $data1['keluar'];
+ <?php echo "Rp ". $data1['keluar']; // untuk mengonect kan query sebagai gaji
  ?>
  </td>
  <td>
  <?php
- echo  "Rp ". ($data ['masuk'] - $data2['Gaji'] - $data1['keluar']); //jika tidak maka tidak akan dikalikan
+ echo  "Rp ". ($data ['masuk'] - $data2['Gaji'] - $data1['keluar']);//rumus //jika tidak maka tidak akan dikalikan
  ?>
  </td>
  
--- total_keuntungan.php
+++ total_keuntungan.php
@@ -1,17 +1,17 @@
 <!--ALWAN ZAKI-->
-<!--Vikri Ammar-->
+<!--Vikri Ammar Kholis 1700018243 (Menambah fitur dan memperbaiki fitur)-->
 <!--A.Z Kharisma-->
 <!--Leonka Pellu-->
 <?php
 	session_start();
 
 if (!isset($_SESSION["login1"])) {
-    	  header("location: http://localhost/apotik-keuangan/login.php");
+    	  header("location: http://localhost/apotik-keuangan/login.php"); //mengonectkan ke pada file login.php
       exit;
     }
-	include "connection/db.php";
-	$QuerySql = "SELECT sum(harga_obat*jumlah_terjual) as total FROM `tabel_penjualan` , `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat";  
-	$QuerySql1 = "SELECT sum(harga_beli*jumlah_obat) AS total FROM `supplier`, `obat` WHERE supplier.kode_obat=obat.kode_obat";
+	include "connection/db.php"; // mengonectkan ke database
+	$QuerySql = "SELECT sum(harga_obat*jumlah_terjual) as total FROM `tabel_penjualan` , `obat` WHERE tabel_penjualan.kode_obat=obat.kode_obat";  //query menjumlah tabel harga obat dan mengkali kan dengan jumlah tabel jumlah terjual sebagai total dari tabel penjualan dan obat di sortir berdasarkan tabel penjualan.kode obat=obat.kode obat 
+	$QuerySql1 = "SELECT sum(harga_beli*jumlah_obat) AS total FROM `supplier`, `obat` WHERE supplier.kode_obat=obat.kode_obat";//query menjumlah tabel harga obat dan mengkali kan dengan jumlah tabel jumlah terjual sebagai total dari tabel penjualan dan obat di sortir berdasarkan tabel penjualan.kode obat=obat.kode obat 
 	$total_pembelian=0;
 	$total_penjualan=0;
 	$SQL = mysqli_query($connect, $QuerySql);
@@ -21,13 +21,13 @@ if (!isset($_SESSION["login1"])) {
 <html>
 <head>
 	<title>DATA KEUNTUNGAN PENJUALAN</title>
-	<link rel="stylesheet" href="bulma.min.css">
+	<link rel="stylesheet" href="bulma.min.css"><!--untuk mengconnect kan ke css -->
 </head>
 <body>
 <?php 
-  include "navbar/navbar_pembelian.php";
+  include "navbar/navbar_pembelian.php"; //ditampil kan di file navbar_pembelian.php
  ?>
-<table class="table is-fullwidth" >
+<table class="table is-fullwidth" > <!--untuk membuat tampilan tabel -->
   <thead>
     <tr>
       <th scope="col"><center>TOTAL KEUNTUNGAN</center></th>
@@ -35,12 +35,12 @@ if (!isset($_SESSION["login1"])) {
   </thead>
 		<?php
 			foreach ($SQL as $key) {
-					$total_penjualan=$key["total"];
+					$total_penjualan=$key["total"]; //mengconnect kan dengan queery 1 di atas 
 				}
 			foreach ($SQL1 as $key) {
-					$total_pembelian=$key["total"];
+					$total_pembelian=$key["total"]; //mengconnect kan dengan queery 2 di atas 
 				}
-				$hasil=$total_penjualan-$total_pembelian;
+				$hasil=$total_penjualan-$total_pembelian; //rumus
 				echo "<tr>
 					<td>$hasil</td>
 				</tr>";
