dcc3f3f9340ca71a0717f5f9794cd9acace76e1a
alfian98 <48217906+alfian98@users.noreply.github.com>
2019-07-24 20:48:02 +0700

    penjelasan script pada index email

--- index_email.php
+++ index_email.php
@@ -2,6 +2,7 @@
  <!DOCTYPE html>
  <html>
  <body>
+  <!-- merupakan code boostrap dan ajax untuk mempercantik tampilan -->
  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
@@ -12,17 +13,20 @@
 <!-- <form action="mail.php" method="post" id="emailForm" enctype="multipart/form-data">
  -->
 
+<!--  merupakan script html tampilan from input data dari penerima email -->
     <div class="form-group">                  
       <label class="control-label col-sm-2" for="fname">Penerima*:</label>
       <div class="col-sm-10">          
         <input type="email" class="form-control" name="penerima" placeholder="Enter email" >
       </div>
     </div>              
+    <!--  merupakan script html tampilan from input data dari penerima nama -->
     <div class="form-group">
       <label class="control-label col-sm-2" for="uname">Subjek*:</label>
       <div class="col-sm-10">
         <input type="text" class="form-control" name="subyek" placeholder="Enter name" >
       </div>
+      <!--  merupakan script html tampilan from input data atau file dari penerima email -->
     </div>
    <div class="form-group">
     <label class="control-label col-sm-2" for="lname">Attach File:</label>
@@ -31,6 +35,7 @@
     </div>
       </div>
     </div>
+    <!--  merupakan script html tampilan from input pesan -->
     <div class="form-group">
       <label class="control-label col-sm-2" for="comment">Message*:</label>
       <div class="col-sm-10">
@@ -41,8 +46,9 @@
   <!-- data yang ditambah -->
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default" name="sendEmail">Send Email</button>
-        <button type="reset" class="btn btn-default" name="reset">Reset</button>
+        <!-- script untuk submit dan reset -->
+        <button type="submit" class="btn btn-default" name="sendEmail" onclick="return confirm('Apakah  ada yakin data ingin di kirim ?')";>Send Email</button>
+        <button type="reset" class="btn btn-default" name="reset"onclick="return confirm('Apakah  ada yakin data ingin di menghapus data?')";>Reset</button>
 
      </div>
     </div>
