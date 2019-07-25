fbd81e93be768e177e45dadec41696f3423def56
Harunsetiaji23 <48378726+Harunsetiaji23@users.noreply.github.com>
2019-07-26 00:38:55 +0700

    Harun Setiaji - UAS-PRPL Kelas E POS
    menambah keterangan kelas dan method nya

--- page_control.php
+++ page_control.php
@@ -1,8 +1,14 @@
 <?php
 
+//Nama	: Harun Setiaji 
+//Nim	: 1700018271
+//Kelas	: E
+
+//Kelas ini digunakan untuk mengakses control dari halaman yang akan diakses
+
 if(!empty($_GET['open'])){$open=trim($_GET['open']);}else{$open="";}
 
-
+//disini menggunakan method switch untuk mengakses ke file seperti home, index, penjualan, dsb
 	switch($open)  
 	{
 		case '' : include 'index.php'; break;
