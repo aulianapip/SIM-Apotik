b6d7fbd1be73b2b048da12f8bd03d34dd546c1e6
putraaditya1877 <48209579+putraaditya1877@users.noreply.github.com>
2019-07-25 16:15:02 +0700

    update data base

--- sim-apotek.sql
+++ sim-apotek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Bulan Mei 2019 pada 14.12
--- Versi server: 10.1.31-MariaDB
--- Versi PHP: 7.2.4
+-- Waktu pembuatan: 25 Jul 2019 pada 11.14
+-- Versi server: 10.3.16-MariaDB
+-- Versi PHP: 7.3.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sim_apotek`
+-- Database: `sim-apotik`
 --
 
 -- --------------------------------------------------------
