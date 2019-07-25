605eea230727387cbba5503112068e4e568a7d59
putraaditya1877 <48209579+putraaditya1877@users.noreply.github.com>
2019-07-25 16:01:45 +0700

    Update terbaru dari inputpasokobat.php

--- inputpasokobat.php
+++ inputpasokobat.php
@@ -18,9 +18,7 @@
         $QuerySql2 = "INSERT INTO barcode VALUES ('$kode_pasok','$kode_obat','$kode_barcode','$tanggal_pasok','$i','$status')";
         mysqli_query($connect, $QuerySql2);
     }
-
   }
-
 ?>
 <!--Aditya Gusti Mandala Putra-->
 <!DOCTYPE html>
@@ -31,53 +29,43 @@
 </head>
 <body>
 <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-   
-
+  <div class="navbar-brand">  
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
       <a class="navbar-item" href="login.html">
       <img src="logut.png"></img>
       </a>
-
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">
           Daftar Tabel
         </a>
-
         <div class="navbar-dropdown">
-          
           <a class="navbar-item" href="dataobat.php">
             Obat
           </a>
-          
           <a class="navbar-item" href="datasupiler.php">
             Supplier
           </a>
-          
         </div>
       </div>
      <a class="navbar-item" href="inputobat.php">
        Tambah Obat
       </a>
-            <a class="navbar-item" href="inputpasokobat.php">
+      <a class="navbar-item" href="inputpasokobat.php">
        Pasok Obat
       </a>
       <a class="navbar-item" href="inputsupiler.php">
        Tambah Supplier
       </a>
-      
   </div>
 </div>
     </div>
-    
   </div> 
 </nav>
 <table class="table is-fullwidth" >
@@ -104,14 +92,14 @@
   <input class="input" type="text" data-length="20" name="jumlah_pasok" placeholder="Jumlah Pasok">
           </div>
         </div>
-                  <div class="control">
+        <div class="control">
   <input class="input" type="text" data-length="20" name="harga_beli" placeholder="Harga Beli">
           </div>
         </div>  
       <div class="control">
   <input class="input" type="text" data-length="20" name="tanggal_pasok" placeholder="Tanggal Pasok">
           </div>
-                <div class="control">
+          <div class="control">
   <input class="input" type="text" data-length="20" name="tanggal_kadaluarsa" placeholder="Tanggal Kadaluarsa">
           </div>
         </div>                               
