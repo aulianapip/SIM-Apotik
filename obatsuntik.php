57071e7c00aa70876bd0c7863e149a0239795d03
gifariii <48229702+gifariii@users.noreply.github.com>
2019-07-25 17:02:44 +0700

    merapikan source code

--- obatsuntik.php
+++ obatsuntik.php
@@ -3,9 +3,7 @@
   $QuerySql = "SELECT * FROM obat inner join jenis_obat on jenis_obat.kode_jenis=obat.kode_jenis where jenis_obat.nama_jenis='Suntik'";
   $SQL = mysqli_query($connect, $QuerySql);
 ?>
-
 <!--Gifari Nanda Utama membuat function sorting data obat suntik-->
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,35 +13,28 @@
 <body>
 <nav class="navbar is-success" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-   
-
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
       <a class="navbar-item" href="login.html">
        <img src="logut.png"></img>
       </a>
-
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">
           Daftar Tabel
         </a>
-
         <div class="navbar-dropdown">
-          
           <a class="navbar-item" href="dataobat.php">
             Obat
           </a>
           <a class="navbar-item" href="datasupiler.php">
             Supiler
           </a>
-    
         </div>
       </div>
 
