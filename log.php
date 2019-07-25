11d907948e7a68df5b211ad8be816d5214ef694f
dimasmaul <48221398+dimasmaul@users.noreply.github.com>
2019-07-26 01:49:57 +0700

    UAS-DIMAS RESTU MAULANA-1700018247

--- log.php
+++ log.php
@@ -1,20 +1,23 @@
-
+//DIMAS RESTU MAULANA
+//E
+//1700018247
+//UAS PRPL
 <?php 
-	if(isset($_POST['kirim'])){
+	if(isset($_POST['kirim'])){ //tombol kirim
 	include 'db.php';
   	$user = $_POST['user'];
   	$password = $_POST['password'];
-  	$QuerySql = "SELECT `user`,'password' from login";
+  	$QuerySql = "SELECT `user`,'password' from login"; //membuat password dari login
   	$SQL = mysqli_query($connect, $QuerySql);
   	foreach ($SQL as $value) {
-  		if($user == "$value[user]"){
-  			if ($password == "$value[password]") {
+  		if($user == "$value[user]"){ //username user
+  			if ($password == "$value[password]") { //password adalah password
   				echo "berhasil";
   				header("Location: dataobat.php");
 
   			}else{
   			header("location: login.html");		 
-  			echo "gagal";
+  			echo "gagal"; //jika gagal akan mengulang login kembali
   			}
   		}else{
 
