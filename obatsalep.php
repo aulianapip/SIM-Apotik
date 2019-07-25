683022c1ef53e52899a2ca791487a6f0139b55dd
gifariii <48229702+gifariii@users.noreply.github.com>
2019-07-25 17:00:30 +0700

    merapikan source code

--- obatsalep.php
+++ obatsalep.php
@@ -4,7 +4,6 @@
   $SQL = mysqli_query($connect, $QuerySql);
 ?>
 <!--Gifari Nanda Utama membuat function sorting data obat salep-->
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,35 +13,28 @@
 <body>
 <nav class="navbar is-success" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-   
-
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
       <a class="navbar-item" href="login.html">
        <img src="logut.png"></img>
       </a>
-
       <div class="navbar-item has-dropdown is-hoverable">
         <a class="navbar-link">
           Daftar Tabel
         </a>
-
         <div class="navbar-dropdown">
-          
           <a class="navbar-item" href="dataobat.php">
             Obat
           </a>
           <a class="navbar-item" href="datasupiler.php">
             Supiler
           </a>
-    
         </div>
       </div>
 
