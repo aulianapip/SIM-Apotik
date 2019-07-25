e49f5efeae09affdf930614a122dc0dadf81b7cb
arindrakurniawan11 <48229794+arindrakurniawan11@users.noreply.github.com>
2019-07-25 14:20:43 +0100

    UAS PRPL Arindra Wahyu Candra 1700018279

--- tidakterdapat.php
+++ tidakterdapat.php
@@ -1,23 +1,5 @@
 <!--UAS PRPL ARINDRA WAHYU  CANDRA KURNIAWAN  Function tidak terdapat obat berfungsi untuk memngetahui obat yang dicari yang tidak ada dalam database -->
 
-<!--1.  Pada kelompok gudang, kami telah membuat beberapa kelas yang mempunyai fungsi sebagai berikut:
-• Fungsi Sorting Nama Obat A-Z : fitur ini berfungsi mengurutkan nama obat sesuai alpabet dari awalan huruf A sampai awalan huruf Z.
-• Fungsi Sorting Jenis Obat Kapsul : fitur ini berfungsi mensorting obat yang berjenis kapsul untuk di tampilkan.
-• Fungsi Menampilkan seluruh data obat
-• Fungsi Tanggal pasok obat
-• Function Update Data Obat : Fitur ini berfungsi mengupdate perubahan yang telah kita tambah, Kurang, dan mengedit sesuai database
-• Function Stok Obat Menipis : Fitur ini berfungsi menandai tabel obat yang stoknya telah menipis
-• Function Pencarian  Data Tidak Ditemukan : fitur ini berfungsi  jika kita mencari obat atau supplier yang tidak ada didatabase
-• Function Sorting Tanggal Supplier : fitur ini berfungsi mensorting obat dengan tanggal pemasok supplier yang telah memasok obat dari tanggal terdahulu
-• Function Tambah Obat : fitur ini berfungsi untuk menambah data obat baru ke dalam tabel Obat
-• Function Tambah Supllier : fitur ini berfungsi untuk menambah data supllier baru ke tabel Supllier
-• Function Cari Obat : fitur ini berfungsi buat mencari data obat yang ada di tabel obat
-• Fuction Cari Supplier : fitur ini berfungsi mncari data supplier yang ada di tabel supplier
-• Function Sorting Nama Supplier A-Z : fitur ini berfungsi untuk mengurutkan nama supplier dari A-Z
-• Function data suplier : fitur ini berfungsi untuk menampilkan data suplier sesuai database
-• Function sorting obat mahal : fitur ini berfungsi untuk mengurutkan harga obat dari yang termahal
--->
-
 <?php
 	include 'db.php';
 	$QuerySql = "SELECT * FROM beli";
@@ -114,6 +96,8 @@
 </nav>
 <table class="table is-fullwidth" >
 <h3 align="center"> TIDAK TERDAPAT DATA </h3>
-	<!--UAS PRPL Menampilkan Tampilkan Pencarian yang tidak terdapat dalam pencarian obat maupun supplier  ARINDRA WAHYU  CANDRA K 1700018279 -->
+	<!--UAS PRPL Menampilkan Tampilkan Pencarian yang tidak terdapat dalam pencarian obat maupun supplier  
+	ARINDRA WAHYU  CANDRA K 1700018279 
+	Function tersebut saat pencarian obat jika obat tidak tersedia dalam database maka dalam pencarian obat tersebut tidak ada-->
 </body>
 </html>
