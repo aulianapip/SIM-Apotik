a062aa0c93637a0c4bfbe83d062b86c54af13254
putraaditya1877 <48209579+putraaditya1877@users.noreply.github.com>
2019-07-25 15:24:40 +0700

    UAS-ADITYA GUSTI MANDALA PUTRA-1700018256

--- kapsul.php
+++ kapsul.php
@@ -1,7 +1,16 @@
+<!--
+UAS PRPRL
+NAMA : Aditya Gusti Mandala Putra
+NIM : 1700018256
+KELAS : E
+
+1. kapsul.php fitur ini berfungsi sebagai menampilkan jenis obat kapsul. obat yang berjenis kapsul pada database akan di tampilkan ketika kita pilih fitur ini pada tampilan.
+-->
+
 <?php
 <--ADITYA GUSTI MANDALA PUTRA YANG BUAT-->
 	include 'db.php';
-	$QuerySql = "SELECT * FROM obat inner join jenis_obat on jenis_obat.kode_jenis=obat.kode_jenis where jenis_obat.nama_jenis='Kapsul'";
+	$QuerySql = "SELECT*FROM obat inner join jenis_obat on jenis_obat.kode_jenis=obat.kode_jenis where jenis_obat.nama_jenis='Kapsul'"; <--2. query di samping sebagai operasi untuk mencari obat yang berjenis kapsul pada tabel obat-->
 	$SQL = mysqli_query($connect, $QuerySql);
 ?>
 <!DOCTYPE html>
@@ -119,7 +128,7 @@
       <th scope="col">Harga Obat</th>
       <th scope="col">Kode Obat</th>
       <th scope="col">Jenis Obat</th>
-      <th scope="col">Kadaluarsa Obat</th>   <!--mohamad rifky fajri perbaiki tabel tanggal kadaluarsa-->
+      <th scope="col">Kadaluarsa Obat</th>
       <th scope="col">Stok Obat</th>
     </tr>
   </thead>
@@ -127,22 +136,22 @@
       foreach ($SQL as $key) {
         if($key['Stok_Obat']<15){
         echo "<tr>
-            <td bgcolor=red>$key[nama_obat]</td>
-            <td bgcolor=red>$key[harga_obat]</td>
-            <td bgcolor=red>$key[kode_obat]</td>
-            <td bgcolor=red>$key[nama_jenis]</td>
-            <td bgcolor=red>$key[kadaluarsa_obat]</td>
-            <td bgcolor=red>$key[Stok_Obat]</td>            
+            <td bgcolor=red>$key[nama_obat]</td><--2. Menampilkan tabel nama obat-->
+            <td bgcolor=red>$key[harga_obat]</td><--2. Menampilkan tabel harga obat-->
+            <td bgcolor=red>$key[kode_obat]</td><--2. Menampilkan tabel kode obat-->
+            <td bgcolor=red>$key[nama_jenis]</td><--2. Menampilkan tabel jenis obat-->
+            <td bgcolor=red>$key[kadaluarsa_obat]</td><--2. Menampilkan tabel kapan kadaluarsa obat-->
+            <td bgcolor=red>$key[Stok_Obat]</td><--2. Menampilkan tabel stok obat yang tersedia-->
         </tr>";
       }
       else{
         echo "<tr>
-            <td>$key[nama_obat]</td>
-            <td>$key[harga_obat]</td>
-            <td>$key[kode_obat]</td>
-            <td>$key[nama_jenis]</td>
-            <td>$key[kadaluarsa_obat]</td>
-            <td>$key[Stok_Obat]</td>            
+            <td>$key[nama_obat]</td><--2. Menampilkan obat pada tabel nama obat-->
+            <td>$key[harga_obat]</td><--2. Menampilkan harga yang tertera pada obat di tabel harga obat-->
+            <td>$key[kode_obat]</td><--2. Menampilkan kode obat di tabel kode obat-->
+            <td>$key[nama_jenis]</td><--2. Menampilkan jenis obat pada tabel jenis obat-->
+            <td>$key[kadaluarsa_obat]</td><--2. Menampilkan tanggal kadaluarsa obat tabel kapan kadaluarsa obat-->
+            <td>$key[Stok_Obat]</td><--2. Menampilkan jumlahh stok obat di tabel stok obat yang tersedia-->            
         </tr>";
       }
       }
