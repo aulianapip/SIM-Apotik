abb863f132e7f683064c6ac95274ded0bb543074
gifariii <48229702+gifariii@users.noreply.github.com>
2019-07-25 16:56:15 +0700

    merapikan source code

--- obatpil.php
+++ obatpil.php
@@ -3,9 +3,7 @@
   $QuerySql = "SELECT * FROM obat inner join jenis_obat on jenis_obat.kode_jenis=obat.kode_jenis where jenis_obat.nama_jenis='Pil'";
   $SQL = mysqli_query($connect, $QuerySql);
 ?>
-
 <!--Gifari Nanda Utama membuat function sorting data obat pil-->
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,15 +13,12 @@
 <body>
 <nav class="navbar is-success" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-   
-
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
       <a class="navbar-item" href="login.html">
