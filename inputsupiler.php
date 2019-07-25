ac7f218710c220a9c784d3c5f98f7d10d8437c10
Sultan Agam <48223826+sultanagam@users.noreply.github.com>
2019-07-25 16:02:30 +0700

    Agam merapihkan source code

--- inputsupiler.php
+++ inputsupiler.php
@@ -2,42 +2,21 @@
   if($_POST){
     include 'db.php';
     $nama_pemasok=$_POST['nama_pemasok'];
-    $kode_obat=$_POST['kode_obat'];
-    $jumlah_pasok=$_POST['jumlah_pasok'];
-    $nomer_telepon_supp=$_POST['nomer_telepon_supp'];
-    $kode_pasok=$_POST['kode_pasok'];
-    $harga_beli=$_POST['harga_beli'];
-    $tanggal_pasok=$_POST['tanggal_pasok'];
+    $nomor_telepon=$_POST['nomor_telepon'];
+    $alamat=$_POST['alamat'];
+    $contact_person=$_POST['contact_person'];
+    $kode_supplier=$_POST['kode_supplier'];
     //memperbaiki query tambah supplier
     
    
-    $QuerySql = " INSERT INTO supplier VALUES ('$nama_pemasok', '$kode_obat', '$jumlah_pasok', '$nomer_telepon_supp', '$kode_pasok', '$harga_beli', '$tanggal_pasok')";
-    $SQL = mysqli_query($connect, $QuerySql);
-	//Query ini untuk menambah data supplier baru ke tabel Supplier
-      	//M. Rafie Sultan Agam 1700018282 
+    $QuerySql = " INSERT INTO supplier VALUES ('$nama_pemasok', '$nomor_telepon', '$alamat', '$contact_person', '$kode_supplier')";
+      $SQL = mysqli_query($connect, $QuerySql);
     }
 
   
 
 ?>
 <!--M. Rafie Sultan Agam-->
-<!--1.  Pada kelompok gudang, kami telah membuat beberapa kelas yang mempunyai fungsi sebagai berikut:
-• Fungsi Sorting Nama Obat A-Z : fitur ini berfungsi mengurutkan nama obat sesuai alpabet dari awalan huruf A sampai awalan huruf Z.
-• Fungsi Sorting Jenis Obat Kapsul : fitur ini berfungsi mensorting obat yang berjenis kapsul untuk di tampilkan.
-• Fungsi Menampilkan seluruh data obat
-• Fungsi Tanggal pasok obat
-• Function Update Data Obat : Fitur ini berfungsi mengupdate perubahan yang telah kita tambah, Kurang, dan mengedit sesuai database
-• Function Stok Obat Menipis : Fitur ini berfungsi menandai tabel obat yang stoknya telah menipis
-• Function Pencarian  Data Tidak Ditemukan : fitur ini berfungsi  jika kita mencari obat atau supplier yang tidak ada didatabase
-• Function Sorting Tanggal Supplier : fitur ini berfungsi mensorting obat dengan tanggal pemasok supplier yang telah memasok obat dari tanggal terdahulu
-• Function Tambah Obat : fitur ini berfungsi untuk menambah data obat baru ke dalam tabel Obat
-• Function Tambah Supllier : fitur ini berfungsi untuk menambah data supllier baru ke tabel Supllier
-• Function Cari Obat : fitur ini berfungsi buat mencari data obat yang ada di tabel obat
-• Fuction Cari Supplier : fitur ini berfungsi mncari data supplier yang ada di tabel supplier
-• Function Sorting Nama Supplier A-Z : fitur ini berfungsi untuk mengurutkan nama supplier dari A-Z
-• Function data suplier : fitur ini berfungsi untuk menampilkan data suplier sesuai database
-• Function sorting obat mahal : fitur ini berfungsi untuk mengurutkan harga obat dari yang termahal
--->
 <!DOCTYPE html>
 <html>
 <head>
@@ -74,13 +53,16 @@
           </a>
           
           <a class="navbar-item" href="datasupiler.php">
-            Supiler
+            Supplier
           </a>
           
         </div>
       </div>
   <a class="navbar-item" href="inputobat.php">
        Tambah Obat
+      </a>
+                  <a class="navbar-item" href="inputpasokobat.php">
+       Pasok Obat
       </a>
       <a class="navbar-item" href="inputsupiler.php">
        Tambah Suppiler
@@ -104,34 +86,24 @@
         </div>
         <div class="row">
           <div class="control">
-  <input class="input" type="text" data-length="20" name="kode_obat" placeholder="Kode Obat">
+  <input class="input" type="text" data-length="20" name="nomor_telepon" placeholder="Nomor Telepon">
           </div>
         </div>
         <div class="row">
           <div class="control">
-  <input class="input" type="text" data-length="20" name="jumlah_pasok" placeholder="Jumlah Pasok">
+  <input class="input" type="text" data-length="20" name="alamat" placeholder="Alamat">
           </div>
         </div>
         <div class="row">
           <div class="control">
-  <input class="input" type="text" data-length="20" name="nomer_telepon_supp" placeholder="Nomer telepon">
+  <input class="input" type="text" data-length="20" name="contact_person" placeholder="Contact Person">
           </div>
         </div>
         <div class="row">
           <div class="control">
-  <input class="input" type="text" data-length="20" name="kode_pasok" placeholder="Kode Pasok">
+  <input class="input" type="text" data-length="20" name="kode_supplier" placeholder="Kode Suppiler">
           </div>
-        </div>
-        <div class="row">
-          <div class="control">
-  <input class="input" type="text" data-length="20" name="harga_beli" placeholder="Harga Beli">
-          </div>
-        </div>        
-        <div class="row">
-          <div class="control">
-  <input class="input" type="text" data-length="20" name="tanggal_pasok" placeholder="Tanggal Pasok">
-          </div>
-        </div>                           
+        </div>                         
         <center><button class="button is-success" type="submit">Submit</button>
         <a href="datasupiler.php" class="button is-success">Lihat Hasil</a></center>
       </form>
