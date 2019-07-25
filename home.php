c711bfe762c1f514c50b25eae96426eea36c9363
falakh15 <falakh1700018227@webmail.uad.ac.id>
2019-07-25 23:42:48 +0700

    UAS-PRPL falakh 1700018227 KELAS E POS

--- home.php
+++ home.php
@@ -1,20 +1,27 @@
-<link href="css/pages/dashboard.css" rel="stylesheet">
 
+<link href="css/pages/dashboard.css" rel="stylesheet">
+<!--NAMA : FALAKH BURHANUDDIN S
+//NIM : 1700018227
+//KELAS : PRPL -E
+
+//Fitur POS, fitur untuk menjalankan fungsi kasir dan operasional penjualan apotik 
+-->
 <div class="main">	
 	<div class="main-inner">
     <div class="container">
 
 
       <?php 
+    //digunakan untuk membedakan akses tiap akun
     switch($levelAkses){
       case "Admin":
-        include"menu_admin.php";
+        include"menu_admin.php";// menuju akses admin
         break;
       case "Kasir":
-        include"menu_kasir.php";
+        include"menu_kasir.php";// menuju akses kasir
         break;
       default:
-        include"menu_all.php";
+        include"menu_all.php";// menuju akses umum
     }
       ?>
 
