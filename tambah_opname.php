fb2895b35b6609febb7204ebea77b22a581f1afa
AirlaIsmail <airlaismail00@gmail.com>
2019-07-25 21:06:33 +0700

    UAS- 1700018251

--- tambah_opname.php
+++ tambah_opname.php
@@ -100,12 +100,6 @@ kami menginputkan status kondisi barang yang berada ditoko, status antara lain a
       $kode_obatB = $_POST['kode_obat'];
       $barcode = "SELECT kode_barcode from barcode WHERE kode_obat = '$kode_obatB' ORDER BY nomor_pasok ASC";
       $cekbarcode = mysqli_query($connect,$barcode);//memilih kode obat obat yang akan ditambah ke data opname(Mail)
-    }elseif (isset($_POST['refresh'])) {
-      header("location: tambah_opname.php");//refresh untuk memunculkan kembali kode obat(Febri)
-    }else{
-      $query = "SELECT kode_obat from obat ";
-      $cekobat = mysqli_query($connect,$query);
-    }//menampilkan kode_obat setelah di refresh(Lussy)
 
    ?>
 <table class="table is-fullwidth" >
