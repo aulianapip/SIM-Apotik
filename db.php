5ee4eb112fcad8bc6d474df079f281ea81a597c3
putraaditya1877 <48209579+putraaditya1877@users.noreply.github.com>
2019-07-25 16:03:54 +0700

    UAS-ADITYA GUSTI MANDALA PUTRA-1700018256

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ KELAS : E
 
 1. db.php fitur ini berfungsi sebagai 'connect' antara database dan fungsi query di setiap file php 
 
-2. dibawah ini merupakan source connect database. 
+2. dibawah ini merupakan source connect database.--> 
 <?php
 $connect = new mysqli("localhost", "root", "", "sim-apotik");
 ?>
