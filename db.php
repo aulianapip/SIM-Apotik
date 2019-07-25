09a8d7bb757b3bee7b23c1de01feeb3320c7cf83
putraaditya1877 <48209579+putraaditya1877@users.noreply.github.com>
2019-07-25 15:58:27 +0700

    UAS-ADITYA GUSTI MANDALA PUTRA-1700018256

--- db.php
+++ db.php
@@ -1,4 +1,12 @@
-<!--Aditya Gusti Mandala Putra--> 
+<!--
+UAS PRPRL
+NAMA : Aditya Gusti Mandala Putra
+NIM : 1700018256
+KELAS : E
+
+1. db.php fitur ini berfungsi sebagai 'connect' antara database dan fungsi query di setiap file php 
+
+2. dibawah ini merupakan source connect database. 
 <?php
-$connect = new mysqli("localhost", "root", "", "SIM-Apotek");
+$connect = new mysqli("localhost", "root", "", "sim-apotik");
 ?>
