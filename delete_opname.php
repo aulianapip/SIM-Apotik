0aa7592891b1eec99217dca5ffbf224c9104663b
zatoni29 <thobiedjailani@gmail.com>
2019-07-30 22:32:22 +0700

    Update delete_opname.php

--- delete_opname.php
+++ delete_opname.php
@@ -1,9 +1,7 @@
- <!-- Alwan Zaki 1700018259 !-->
- <!--no1. Stock Opname adalah kegiatan perhitungan secara fisik atas persediaan barang di gudang 
-secara fisik atas persedian barang di gudang yang akan dijual.Pada fitur yang kami buat
-kami menginputkan status kondisi barang yang berada ditoko, status antara lain adalah digudang rusak, hilang ,di pinjam , dan terjual. jika terjadi kesalahan input status maka dapat diubah dengan fitur edit, dan bila barang telah kembali atau di ganti atau telah di konfirmasi oleh pihak gudang dan kasir maka data opname dapat di hapus dengan fitur delete  -->
+<!-- Thobie Zatoni 1700018236 !-->
  <?php
-include 'db.php';
+include 'db.php';//Menghubungkan ke file yang bersambung dengan database
+
 // menyimpan data id kedalam variabel
 $kode_barcode   = $_GET['kode_barcode'];
 // query SQL untuk insert data
